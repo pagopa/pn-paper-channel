bca666c22f3fcb523da0b65dfcc3f81480bede0a
cristopher.moccia <cristopher.moccia@accenture.com>
2022-11-24 10:56:25 +0100

    added liquibase directories configs, created the skeleton of the project

---
+++ src/main/java/it/pagopa/pn/paperchannel/PaperChannelApplication.java
@@ -0,0 +1,13 @@
+package it.pagopa.pn.paperchannel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperChannelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaperChannelApplication.class, args);
+    }
+
+}
