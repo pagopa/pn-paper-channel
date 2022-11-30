e1989dbed623980ea0843b08dbec6922c96b6b6b
ACT3PRQ <davide.fragione@accenture.com>
2022-11-30 17:11:25 +0100

    Implementazione swagger openApi FrontEnd

---
+++ src/main/java/it/pagopa/pn/paperchannel/pojo/Address.java
@@ -0,0 +1,31 @@
+package it.pagopa.pn.paperchannel.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+
+    private String fullName;
+
+    private String nameRow2;
+
+    private String address;
+
+    private String addressRow2;
+
+    private String cap;
+
+    private String city;
+
+    private String city2;
+
+    private String pr;
+
+    private String country;
+}
