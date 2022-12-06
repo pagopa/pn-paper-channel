a06ec32c262f57203f9944189ba7f1d715ab8d22
arianna.zafarana <arianna.zafarana@accenture.com>
2022-12-06 11:26:27 +0100

    comment sqs configuration

--- src/main/java/it/pagopa/pn/paperchannel/config/SQSConfig.java
+++ src/main/java/it/pagopa/pn/paperchannel/config/SQSConfig.java
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.util.StringUtils;
 import software.amazon.awssdk.services.sqs.SqsClient;
 
-@Configuration
+//@Configuration
 @Slf4j
 public class SQSConfig {
 
@@ -30,24 +30,26 @@ public class SQSConfig {
      * https://docs.awspring.io/spring-cloud-aws/docs/2.4.2/reference/html/index.html#fifo-queue-support
      * @return bean per le code
      */
-    @Bean
+    //@Bean
     public DeliveryMomProducer deliveryMomProducer(SqsClient sqsClient, ObjectMapper objectMapper){
-        return new DeliveryMomProducer(sqsClient,this.awsConfigs.getReadyDeliveryQueue(),objectMapper, DeliveryEvent.class);
+        //return new DeliveryMomProducer(sqsClient,this.awsConfigs.getReadyDeliveryQueue(),objectMapper, DeliveryEvent.class);
+        return null;
     }
 
-    @Bean
+    //@Bean
     public AmazonSQSAsync amazonSQS() {
         log.info(awsConfigs.getRegionCode());
-        if (StringUtils.hasText(awsConfigs.getEndpointUrl())){
-
-            log.info("with endpoint");
-            return AmazonSQSAsyncClientBuilder.standard()
-                    .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(awsConfigs.getEndpointUrl(), awsConfigs.getRegionCode()))
-                    .build();}
-        else {
-            return AmazonSQSAsyncClientBuilder.standard()
-                    .withRegion(awsConfigs.getRegionCode())
-                    .build();
-        }
+//        if (StringUtils.hasText(awsConfigs.getEndpointUrl())){
+//
+//            log.info("with endpoint");
+//            return AmazonSQSAsyncClientBuilder.standard()
+//                    .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(awsConfigs.getEndpointUrl(), awsConfigs.getRegionCode()))
+//                    .build();}
+//        else {
+//            return AmazonSQSAsyncClientBuilder.standard()
+//                    .withRegion(awsConfigs.getRegionCode())
+//                    .build();
+//        }
+        return null;
     }
 }
--- src/main/java/it/pagopa/pn/paperchannel/service/impl/SqsQueueSender.java
+++ src/main/java/it/pagopa/pn/paperchannel/service/impl/SqsQueueSender.java
@@ -1,38 +1,30 @@
 package it.pagopa.pn.paperchannel.service.impl;
 
-import it.pagopa.pn.api.dto.events.GenericEventHeader;
-import it.pagopa.pn.paperchannel.queue.action.DeliveryMomProducer;
-import it.pagopa.pn.paperchannel.queue.model.DeliveryEvent;
-import it.pagopa.pn.paperchannel.queue.model.DeliveryPayload;
 import it.pagopa.pn.paperchannel.queue.model.EventTypeEnum;
 import it.pagopa.pn.paperchannel.service.SqsSender;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.time.Instant;
-import java.util.UUID;
 
 @Service
 @Slf4j
 public class SqsQueueSender implements SqsSender {
 
-    @Autowired
-    private DeliveryMomProducer deliveryMomProducer;
+//    @Autowired
+//    private DeliveryMomProducer deliveryMomProducer;
 
     @Override
     public void pushEvent(EventTypeEnum eventType){
-        GenericEventHeader deliveryHeader= GenericEventHeader.builder()
-                .publisher("paper-channel-update")
-                .eventId(UUID.randomUUID().toString())
-                .createdAt(Instant.now())
-                .eventType(eventType.name())
-                .build();
+//        GenericEventHeader deliveryHeader= GenericEventHeader.builder()
+//                .publisher("paper-channel-update")
+//                .eventId(UUID.randomUUID().toString())
+//                .createdAt(Instant.now())
+//                .eventType(eventType.name())
+//                .build();
+//
+//        DeliveryPayload deliveryPayload= new DeliveryPayload("delivery Event body");
+//
+//        DeliveryEvent deliveryEvent=new DeliveryEvent(deliveryHeader,deliveryPayload);
 
-        DeliveryPayload deliveryPayload= new DeliveryPayload("delivery Event body");
-
-        DeliveryEvent deliveryEvent=new DeliveryEvent(deliveryHeader,deliveryPayload);
-
-        this.deliveryMomProducer.push(deliveryEvent);
+        // this.deliveryMomProducer.push(deliveryEvent);
     }
 }
