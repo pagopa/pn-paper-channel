420bc75713b3972d0b6a65eda2c0995abacf35fd
arianna.zafarana <arianna.zafarana@accenture.com>
2022-12-06 15:25:09 +0100

    comment sqs configuration

--- src/main/java/it/pagopa/pn/paperchannel/queue/action/QueueListener.java
+++ src/main/java/it/pagopa/pn/paperchannel/queue/action/QueueListener.java
@@ -1,17 +1,9 @@
 package it.pagopa.pn.paperchannel.queue.action;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.awspring.cloud.messaging.listener.SqsMessageDeletionPolicy;
-import io.awspring.cloud.messaging.listener.annotation.SqsListener;
-import it.pagopa.pn.paperchannel.queue.model.DeliveryPayload;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.handler.annotation.Headers;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
-
-import java.util.Map;
 
 @Component
 @Slf4j
@@ -21,20 +13,20 @@ public class QueueListener {
     private ObjectMapper objectMapper;
 
     //@SqsListener(value = "${aws.ready-delivery-queue}",deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
-    public void pull(@Payload String node, @Headers Map<String,Object> headers){
-
-        convertPayload(node);
-        log.info("BODY - {}",node);
-        log.info("HEADERS - {}",headers);
-    }
-
-    private DeliveryPayload convertPayload(String json){
-        try {
-            return objectMapper.readValue(json,DeliveryPayload.class);
-
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
+//    public void pull(@Payload String node, @Headers Map<String,Object> headers){
+//
+//        convertPayload(node);
+//        log.info("BODY - {}",node);
+//        log.info("HEADERS - {}",headers);
+//    }
+//
+//    private DeliveryPayload convertPayload(String json){
+//        try {
+//            return objectMapper.readValue(json,DeliveryPayload.class);
+//
+//        } catch (JsonProcessingException e) {
+//            e.printStackTrace();
+//            return null;
+//        }
+//    }
 }
