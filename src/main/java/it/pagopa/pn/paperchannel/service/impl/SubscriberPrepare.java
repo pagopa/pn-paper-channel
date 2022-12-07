e7847e2c60089b8bff542b3c1ddcbac2a8a3f51b
ACT3PRQ <davide.fragione@accenture.com>
2022-12-07 15:17:15 +0100

    Implementazione PnGenericException

--- src/main/java/it/pagopa/pn/paperchannel/service/impl/SubscriberPrepare.java
+++ src/main/java/it/pagopa/pn/paperchannel/service/impl/SubscriberPrepare.java
@@ -1,6 +1,9 @@
 package it.pagopa.pn.paperchannel.service.impl;
 
 
+import it.pagopa.pn.paperchannel.exception.ExceptionTypeEnum;
+import it.pagopa.pn.paperchannel.exception.PnGenericException;
+import it.pagopa.pn.paperchannel.middleware.db.dao.RequestDeliveryDAO;
 import it.pagopa.pn.paperchannel.queue.model.DeliveryPayload;
 import lombok.extern.slf4j.Slf4j;
 import org.reactivestreams.Subscriber;
@@ -11,9 +14,11 @@ public class SubscriberPrepare implements Subscriber<DeliveryPayload> {
 
     private DeliveryPayload deliveryPayload;
     private SqsQueueSender sqsQueueSender;
+    private RequestDeliveryDAO requestDeliveryDAO;
 
-    public SubscriberPrepare(SqsQueueSender sqsQueueSender) {
+    public SubscriberPrepare(SqsQueueSender sqsQueueSender,RequestDeliveryDAO requestDeliveryDAO) {
         this.sqsQueueSender = sqsQueueSender;
+        this.requestDeliveryDAO = requestDeliveryDAO;
     }
 
     @Override
@@ -30,7 +35,14 @@ public class SubscriberPrepare implements Subscriber<DeliveryPayload> {
 
     @Override
     public void onError(Throwable throwable) {
+
         log.error("on Error : {}", throwable.getMessage());
+        if(throwable instanceof PnGenericException){
+            PnGenericException exception = (PnGenericException) throwable;
+            if(exception.getExceptionType().equals(ExceptionTypeEnum.UNTRACEABLE_ADDRESS)){
+
+            }
+        }
     }
 
     @Override
