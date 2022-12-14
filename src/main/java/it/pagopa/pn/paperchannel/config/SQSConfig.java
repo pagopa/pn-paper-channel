67b5f0c866853a521fe03a8f956e658fef2011ac
ariannazafarana <112564573+ariannazafarana@users.noreply.github.com>
2022-12-14 14:43:13 +0100

    adds log

--- src/main/java/it/pagopa/pn/paperchannel/config/SQSConfig.java
+++ src/main/java/it/pagopa/pn/paperchannel/config/SQSConfig.java
@@ -33,6 +33,10 @@ public class SQSConfig {
      */
     @Bean
     public DeliveryMomProducer deliveryMomProducer(SqsClient sqsClient, ObjectMapper objectMapper){
+        log.info("Params: "+ this.pnPaperChannelConfig.getQueueDeliveryPush());
+        log.info("Params: "+ this.pnPaperChannelConfig.getSafeStorageCxId());
+        log.info("Params: "+ this.pnPaperChannelConfig.getClientNationalRegistriesBasepath());
+
         return new DeliveryMomProducer(sqsClient,this.pnPaperChannelConfig.getQueueDeliveryPush(),objectMapper, DeliveryEvent.class);
     }
 
