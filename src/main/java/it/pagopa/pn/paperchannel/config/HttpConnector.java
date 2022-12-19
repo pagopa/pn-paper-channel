1504b683f89b3d2c4acfd75d47a2d2381f67ed75
ACT3PRQ <davide.fragione@accenture.com>
2022-12-19 17:47:16 +0100

    fix downloadFile

--- src/main/java/it/pagopa/pn/paperchannel/config/HttpConnector.java
+++ src/main/java/it/pagopa/pn/paperchannel/config/HttpConnector.java
@@ -1,10 +1,9 @@
 package it.pagopa.pn.paperchannel.config;
 
-import okhttp3.Call;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.io.IOException;
 
@@ -14,20 +13,18 @@ public class HttpConnector {
         throw new IllegalCallerException("the constructor must not called");
     }
 
-
-    public static PDDocument downloadFile(String url) throws IOException {
-        OkHttpClient client = new OkHttpClient().newBuilder()
-                .build();
-        Request request = new Request.Builder()
-                .url(url)
-                .addHeader("Content-Type", "application/json")
-                .addHeader("Accept", "application/json")
-                .build();
-        Call call = client.newCall(request);
-        Response response = call.execute();
-        assert response.body() != null;
-        PDDocument document = PDDocument.load(response.body().byteStream());
-        response.close();
-        return document;
+    private static Mono<PDDocument> downloadFile(String url) {
+        return WebClient.create(url)
+                .get()
+                .accept(MediaType.APPLICATION_PDF)
+                .retrieve()
+                .bodyToMono(byte[].class)
+                .flatMap(bytes -> {
+                    try {
+                        return Mono.just(PDDocument.load(bytes));
+                    } catch (IOException e) {
+                        return Mono.error(e);
+                    }
+                });
     }
 }
