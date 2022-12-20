e5c774818fa536afdbc82a0090110a25689bd861
elena.ibba <elena.ibba@accenture.com>
2022-12-20 16:39:53 +0100

    fixed getFile

--- src/main/java/it/pagopa/pn/paperchannel/service/impl/PrepareAsyncServiceImpl.java
+++ src/main/java/it/pagopa/pn/paperchannel/service/impl/PrepareAsyncServiceImpl.java
@@ -1,6 +1,8 @@
 package it.pagopa.pn.paperchannel.service.impl;
 
+import com.sun.jdi.LongValue;
 import it.pagopa.pn.paperchannel.config.HttpConnector;
+import it.pagopa.pn.paperchannel.exception.ExceptionTypeEnum;
 import it.pagopa.pn.paperchannel.exception.PnGenericException;
 import it.pagopa.pn.paperchannel.exception.PnRetryStorageException;
 import it.pagopa.pn.paperchannel.mapper.AddressMapper;
@@ -13,6 +15,7 @@ import it.pagopa.pn.paperchannel.model.Address;
 import it.pagopa.pn.paperchannel.model.AttachmentInfo;
 import it.pagopa.pn.paperchannel.model.Contract;
 import it.pagopa.pn.paperchannel.model.DeliveryAsyncModel;
+import it.pagopa.pn.paperchannel.msclient.generated.pnsafestorage.v1.dto.FileDownloadResponseDto;
 import it.pagopa.pn.paperchannel.service.PaperAsyncService;
 import it.pagopa.pn.paperchannel.utils.DateUtils;
 import lombok.extern.slf4j.Slf4j;
@@ -20,9 +23,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.ParallelFlux;
 import reactor.util.retry.Retry;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.time.Duration;
 import java.util.stream.Collectors;
 
@@ -84,17 +89,30 @@ public class PrepareAsyncServiceImpl implements PaperAsyncService {
         return Mono.just(new Contract(5.0, 10.0));
     }
 
+
+
+    public Mono<FileDownloadResponseDto> getFileRecursive(Integer n, String fileKey, BigDecimal millis){
+        if (n<0)
+            return Mono.error(new PnGenericException( DOCUMENT_URL_NOT_FOUND, DOCUMENT_URL_NOT_FOUND.getMessage() ) );
+        else {
+             return Mono.just ("").delay(Duration.ofMillis( millis.longValue() ))
+                     .flatMap(item -> safeStorageClient.getFile(fileKey)
+                     .map(fileDownloadResponseDto -> fileDownloadResponseDto)
+                             .onErrorResume(ex -> {
+                                 log.error (ex.getMessage());
+                                 return Mono.error(ex);
+                             })
+                     .onErrorResume(PnRetryStorageException.class, ex ->
+                         getFileRecursive(n - 1, fileKey, ex.getRetryAfter())
+                    ));
+        }
+    }
+
     private Mono<DeliveryAsyncModel> getAttachmentsInfo(DeliveryAsyncModel deliveryAsyncModel){
         return Flux.fromStream(deliveryAsyncModel.getAttachments().stream())
                 .parallel()
-                .flatMap(attachment -> safeStorageClient.getFile(attachment.getFileKey())
-                        .retryWhen(
-                                Retry.backoff(3, Duration.ofMillis(500))
-                                        .filter(PnRetryStorageException.class::isInstance)
-                        )
-                )
+                .flatMap( attachment -> getFileRecursive(3, attachment.getFileKey(), new BigDecimal(0)))
                 .flatMap(fileResponse -> {
-
                     AttachmentInfo info = AttachmentMapper.fromSafeStorage(fileResponse);
                     if (info.getUrl() == null)
                         throw new PnGenericException(DOCUMENT_URL_NOT_FOUND, DOCUMENT_URL_NOT_FOUND.getMessage());
