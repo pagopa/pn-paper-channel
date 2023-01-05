54f518bb849a722afa7ce3f3c6db0151e4f07c99
ariannazafarana <112564573+ariannazafarana@users.noreply.github.com>
2023-01-05 09:46:45 +0100

    fix POST execution api

--- src/main/java/it/pagopa/pn/paperchannel/service/impl/PaperMessagesServiceImpl.java
+++ src/main/java/it/pagopa/pn/paperchannel/service/impl/PaperMessagesServiceImpl.java
@@ -13,12 +13,13 @@ import it.pagopa.pn.paperchannel.model.Address;
 import it.pagopa.pn.paperchannel.model.AttachmentInfo;
 import it.pagopa.pn.paperchannel.rest.v1.dto.*;
 import it.pagopa.pn.paperchannel.service.PaperMessagesService;
-import it.pagopa.pn.paperchannel.validator.PrepareRequestValidator;
 import it.pagopa.pn.paperchannel.service.SqsSender;
+import it.pagopa.pn.paperchannel.validator.PrepareRequestValidator;
 import it.pagopa.pn.paperchannel.validator.SendRequestValidator;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
@@ -51,7 +52,7 @@ public class PaperMessagesServiceImpl extends BaseService implements PaperMessag
         return requestDeliveryDAO.getByRequestId(requestId)
                 .zipWhen(entity -> addressDAO.findByRequestId(requestId).map(address -> address))
                 .map(entityAndAddress -> PrepareEventMapper.fromResult(entityAndAddress.getT1(),entityAndAddress.getT2()))
-                .switchIfEmpty(Mono.error(new PnGenericException(DELIVERY_REQUEST_NOT_EXIST, DELIVERY_REQUEST_NOT_EXIST.getMessage())));
+                .switchIfEmpty(Mono.error(new PnGenericException(DELIVERY_REQUEST_NOT_EXIST, DELIVERY_REQUEST_NOT_EXIST.getMessage(), HttpStatus.NOT_FOUND)));
     }
 
     @Override
@@ -65,7 +66,8 @@ public class PaperMessagesServiceImpl extends BaseService implements PaperMessag
                     Address address = AddressMapper.fromAnalogToAddress(sendRequest.getReceiverAddress());
                     return super.calculator(attachments, address, sendRequest.getProductType()).map(value -> value);
                 })
-                .zipWith(this.externalChannelClient.sendEngageRequest(sendRequest).map(item -> Mono.just("")), (amount, none) -> amount)
+                .switchIfEmpty(Mono.error(new PnGenericException(DELIVERY_REQUEST_NOT_EXIST, DELIVERY_REQUEST_NOT_EXIST.getMessage(), HttpStatus.NOT_FOUND)))
+                .zipWith(this.externalChannelClient.sendEngageRequest(sendRequest).then(Mono.just("")), (amount, none) -> amount)
                 .map(amount -> {
                     log.info("Amount for response : {}", amount);
                     SendResponse sendResponse = new SendResponse();
@@ -127,7 +129,7 @@ public class PaperMessagesServiceImpl extends BaseService implements PaperMessag
                             );
 
                 })
-                .switchIfEmpty(Mono.error(new PnGenericException(DELIVERY_REQUEST_NOT_EXIST, DELIVERY_REQUEST_NOT_EXIST.getMessage())));
+                .switchIfEmpty(Mono.error(new PnGenericException(DELIVERY_REQUEST_NOT_EXIST, DELIVERY_REQUEST_NOT_EXIST.getMessage(), HttpStatus.NOT_FOUND)));
 
     }
 
