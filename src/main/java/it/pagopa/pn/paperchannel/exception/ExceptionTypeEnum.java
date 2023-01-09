cad19276835b93edc63b07e06bcc7c63427b27f1
ariannazafarana <112564573+ariannazafarana@users.noreply.github.com>
2023-01-09 09:43:45 +0100

    no change just for build

--- src/main/java/it/pagopa/pn/paperchannel/exception/ExceptionTypeEnum.java
+++ src/main/java/it/pagopa/pn/paperchannel/exception/ExceptionTypeEnum.java
@@ -10,11 +10,9 @@ public enum ExceptionTypeEnum{
     DOCUMENT_URL_NOT_FOUND("DOCUMENT_URL_NOT_FOUND", "Url allegato non disponibile"),
     DOCUMENT_NOT_DOWNLOADED("DOCUMENT_NOT_DOWNLOADED", "Non è stato possibile scaricare il documento"),
     DIFFERENT_DATA_REQUEST("DIFFERENT_DATA_REQUEST", "Richiesta già preso in carico ma sono state inviate informazioni differenti (requestId già presente)"),
-
     COUNTRY_NOT_FOUND("COUNTRY NOT FOUND", "Il paese non è stato trovato"),
     COST_NOT_FOUND("COST NOT FOUND", "Il costo non è stato trovato"),
-    DIFFERENT_DATA_RESULT("DIFFERENT_DATA_RESULT", "I dati restituiti dal servizio External Channel sono differenti rispetto a quelli memorizzati nel database (per uno stesso requestId)")
-    ;
+    DIFFERENT_DATA_RESULT("DIFFERENT_DATA_RESULT", "I dati restituiti dal servizio External Channel sono differenti rispetto a quelli memorizzati nel database (per uno stesso requestId)");
 
     private final String title;
     private final String message;
