c38af649f7824fd9147b8b6c1fc24f9a35291e19
federica.bellini <ACT3UI0@posteitaliane.it>
2023-01-13 16:16:09 +0100

    implemented junit test for QueueListener

--- src/test/java/it/pagopa/pn/paperchannel/middleware/queue/consumer/QueueListenerTest.java
+++ src/test/java/it/pagopa/pn/paperchannel/middleware/queue/consumer/QueueListenerTest.java
@@ -8,6 +8,7 @@ import it.pagopa.pn.paperchannel.service.PaperAsyncService;
 import it.pagopa.pn.paperchannel.service.PaperResultAsyncService;
 import it.pagopa.pn.paperchannel.service.SqsSender;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -29,6 +30,7 @@ class QueueListenerTest extends BaseTest {
     private QueueListener queueListener;
     @Mock
     private PaperAsyncService paperAsyncService;
+
     @Mock
     private SqsSender sender;
     @Mock
@@ -74,5 +76,49 @@ class QueueListenerTest extends BaseTest {
         assertTrue(true);
     }
 
+    @Test
+    void pullNationalRegistriesJsonBadlyTest(){
+        String json = "{}";
+        try {
+            queueListener.pullNationalRegistries(json, new HashMap<>());
+            fail("Error with pull. Missed an exception");
+        }
+        catch (PnGenericException ex){
+            assertNotNull(ex);
+            assertNotNull(ex.getExceptionType());
+            assertEquals(UNTRACEABLE_ADDRESS, ex.getExceptionType());
+        }
+    }
+
+    @Test
+    void pullNationalRegistriesThrowErrorTest(){
+        String json = "{\"correlationId\":\"\",\"taxId\":\"CODICEFISCALE200\",\"digitalAddress\":null,\"physicalAddress\":{\"at\":\"MarioRossi\",\"address\":\"ViaAldoMoro\",\"addressDetails\":\"39\",\"zip\":\"21047\",\"municipality\":\"Saronno\",\"municipalityDetails\":\"Test\",\"province\":\"VA\",\"foreignState\":\"Italy\"}}";
+        try {
+            queueListener.pullNationalRegistries(json, new HashMap<>());
+            fail("Error with pull. Missed an exception");
+        }
+        catch (PnGenericException ex){
+            assertNotNull(ex);
+            assertNotNull(ex.getExceptionType());
+            assertEquals(UNTRACEABLE_ADDRESS, ex.getExceptionType());
+
+        }
+    }
+
+    @Test
+    void pullNationalRegistriesOkTest(){
+        String json = "{\"correlationId\":\"string\",\"taxId\":\"CODICEFISCALE200\",\"digitalAddress\":null,\"physicalAddress\":{\"at\":\"MarioRossi\",\"address\":\"ViaAldoMoro\",\"addressDetails\":\"39\",\"zip\":\"21047\",\"municipality\":\"Saronno\",\"municipalityDetails\":\"Test\",\"province\":\"VA\",\"foreignState\":\"Italy\"}}";
+        Mockito.doNothing().when(sender).pushToInternalQueue(Mockito.any());
+        queueListener.pullNationalRegistries(json, new HashMap<>());
+        assertTrue(true);
+
+    }
+    @Test
+    void pullExternalChannelOkTest(){
+        String json = "{\"digitalCourtesy\": null, \"digitalLegal\": null, \"analogMail\": { \"requestId\": \"AKUZ-AWPL-LTPX-20230415\", \"registeredLetterCode\": null, \"productType\": \"AR\", \"iun\": \"AKUZ-AWPL-LTPX-20230415\", \"statusCode\": \"002\", \"statusDescription\": \"Mock status\", \"statusDateTime\": \"2023-01-12T14:35:35.135725152Z\", \"deliveryFailureCause\": null, \"attachments\": null, \"discoveredAddress\": null, \"clientRequestTimeStamp\": \"2023-01-12T14:35:35.13572075Z\"}}";
+        Mockito.when(paperResultAsyncService.resultAsyncBackground(Mockito.any())).thenReturn(Mono.just(new PnDeliveryRequest()));
+        queueListener.pullExternalChannel(json, new HashMap<>());
+        assertTrue(true);
+    }
 
 }
