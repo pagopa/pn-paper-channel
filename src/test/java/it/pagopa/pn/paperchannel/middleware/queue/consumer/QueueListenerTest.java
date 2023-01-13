f547e65ccfa7b1647ada31266fea4b75ca673763
cristopher.moccia <cristopher.moccia@accenture.com>
2023-01-13 11:03:58 +0100

    added other case test of internalQueue Listener

--- src/test/java/it/pagopa/pn/paperchannel/middleware/queue/consumer/QueueListenerTest.java
+++ src/test/java/it/pagopa/pn/paperchannel/middleware/queue/consumer/QueueListenerTest.java
@@ -2,12 +2,23 @@ package it.pagopa.pn.paperchannel.middleware.queue.consumer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import it.pagopa.pn.paperchannel.config.BaseTest;
+import it.pagopa.pn.paperchannel.exception.PnGenericException;
+import it.pagopa.pn.paperchannel.middleware.db.entities.PnDeliveryRequest;
 import it.pagopa.pn.paperchannel.service.PaperAsyncService;
 import it.pagopa.pn.paperchannel.service.PaperResultAsyncService;
 import it.pagopa.pn.paperchannel.service.SqsSender;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import reactor.core.publisher.Mono;
+
+import java.util.HashMap;
+
+import static it.pagopa.pn.paperchannel.exception.ExceptionTypeEnum.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 @Slf4j
@@ -25,7 +36,41 @@ class QueueListenerTest extends BaseTest {
     private ObjectMapper objectMapper;
 
 
+    @Test
+    void internalQueueJsonBadlyTest(){
+        String json = "{}";
+        try {
+            queueListener.pullFromInternalQueue(json, new HashMap<>());
+            fail("Error with pull. Missed an exception");
+        } catch (PnGenericException ex){
+            assertNotNull(ex);
+            assertNotNull(ex.getExceptionType());
+            assertEquals(MAPPER_ERROR, ex.getExceptionType());
+        }
+    }
 
+
+    @Test
+    void internalQueuePrepareAsyncThrowErrorTest(){
+        String json = "{'correlationId': null,'requestId':'NRTK-EWZL-KVPV-202212-Q-1124ds'}";
+        try {
+            Mockito.when(paperAsyncService.prepareAsync(Mockito.any())).thenThrow(new PnGenericException(DOCUMENT_URL_NOT_FOUND, DOCUMENT_URL_NOT_FOUND.getMessage()));
+            queueListener.pullFromInternalQueue(json, new HashMap<>());
+            fail("Error with pull. Missed an exception");
+        } catch (PnGenericException ex){
+            assertNotNull(ex);
+            assertNotNull(ex.getExceptionType());
+            assertEquals(PREPARE_ASYNC_LISTENER_EXCEPTION, ex.getExceptionType());
+        }
+    }
+
+    @Test
+    void internalQueueOKTest(){
+        String json = "{'correlationId': null,'requestId':'NRTK-EWZL-KVPV-202212-Q-1124ds'}";
+        Mockito.when(paperAsyncService.prepareAsync(Mockito.any())).thenReturn(Mono.just(new PnDeliveryRequest()));
+        queueListener.pullFromInternalQueue(json, new HashMap<>());
+        assertTrue(true);
+    }
 
 
 }
