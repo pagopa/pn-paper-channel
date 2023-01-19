ac85d61f9b23c89c9d7da3668fe5d0e01fda8c49
cristopher.moccia <cristopher.moccia@accenture.com>
2023-01-19 10:33:16 +0100

    fixed flow of pageble DeliveryDriver

--- src/main/java/it/pagopa/pn/paperchannel/rest/v1/PaperChannelRestV1Controller.java
+++ src/main/java/it/pagopa/pn/paperchannel/rest/v1/PaperChannelRestV1Controller.java
@@ -1,14 +1,33 @@
 package it.pagopa.pn.paperchannel.rest.v1;
 
 import it.pagopa.pn.paperchannel.rest.v1.api.DeliveryDriverApi;
+import it.pagopa.pn.paperchannel.rest.v1.dto.AllPricesContractorResponseDto;
+import it.pagopa.pn.paperchannel.rest.v1.dto.PageableDeliveryDriverResponseDto;
+import it.pagopa.pn.paperchannel.rest.v1.dto.PageableTenderResponseDto;
 import it.pagopa.pn.paperchannel.service.PaperChannelService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class PaperChannelRestV1Controller implements DeliveryDriverApi {
     @Autowired
     private PaperChannelService paperChannelService;
 
+    @Override
+    public Mono<ResponseEntity<PageableTenderResponseDto>> takeTender(Integer page, Integer size, ServerWebExchange exchange) {
+        return DeliveryDriverApi.super.takeTender(page, size, exchange);
+    }
 
+    @Override
+    public Mono<ResponseEntity<PageableDeliveryDriverResponseDto>> takeDeliveriesDrivers(String tenderCode, Integer page, Integer size, ServerWebExchange exchange) {
+        return DeliveryDriverApi.super._takeDeliveriesDrivers(tenderCode, page, size, exchange);
+    }
+
+    @Override
+    public Mono<ResponseEntity<AllPricesContractorResponseDto>> takePrices(String tenderCode, String deliveryDriverId, ServerWebExchange exchange) {
+        return DeliveryDriverApi.super.takePrices(tenderCode, deliveryDriverId, exchange);
+    }
 }
--- src/main/java/it/pagopa/pn/paperchannel/service/impl/PaperChannelServiceImpl.java
+++ src/main/java/it/pagopa/pn/paperchannel/service/impl/PaperChannelServiceImpl.java
@@ -1,11 +1,18 @@
 package it.pagopa.pn.paperchannel.service.impl;
 
+import it.pagopa.pn.paperchannel.mapper.DeliveryDriverMapper;
 import it.pagopa.pn.paperchannel.middleware.db.dao.CostDAO;
 import it.pagopa.pn.paperchannel.middleware.db.dao.DeliveryDriverDAO;
+import it.pagopa.pn.paperchannel.rest.v1.dto.AllPricesContractorResponseDto;
+import it.pagopa.pn.paperchannel.rest.v1.dto.PageableDeliveryDriverResponseDto;
+import it.pagopa.pn.paperchannel.rest.v1.dto.PageableTenderResponseDto;
 import it.pagopa.pn.paperchannel.service.PaperChannelService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
 @Slf4j
 @Service
@@ -16,6 +23,27 @@ public class PaperChannelServiceImpl implements PaperChannelService {
 
     @Autowired
     private DeliveryDriverDAO deliveryDriverDAO;
+
+    @Override
+    public Mono<PageableTenderResponseDto> getAllTender(Integer page, Integer size) {
+        return null;
+    }
+
+    @Override
+    public Mono<PageableDeliveryDriverResponseDto> getAllDeliveriesDrivers(String tenderCode, Integer page, Integer size) {
+        Pageable pageable = PageRequest.of(page-1, size);
+        return deliveryDriverDAO.getDeliveryDriver(tenderCode)
+                .map(list -> DeliveryDriverMapper.toPagination(pageable, list))
+                .map(DeliveryDriverMapper::toPageableResponse);
+    }
+
+    @Override
+    public Mono<AllPricesContractorResponseDto> getAllPricesOfDeliveryDriver(String tenderCode, String deliveryDriver) {
+        return null;
+    }
+
+
+
 
 
     /*
