4de88d0917415f28dcda8037645f452f63386db0
francarl77 <francesco.carlucci@accenture.com>
2023-02-10 18:29:01 +0100

    add scripts for caps and zones loading

---
+++ scripts/aws/migration/load-zones.sh
@@ -0,0 +1,21 @@
+tail -n +1 zones.csv | parallel --pipe -N25 'head -c -1 | jq --slurp --raw-input "{ \"pn-PaperZone\": split(\"\n\") | map(split(\",\")) |
+    map(
+		{
+			\"PutRequest\": {
+				\"Item\": {
+					\"zone\": {
+						\"S\": .[0]
+					},
+					\"countryIt\": {
+						\"S\": .[1]
+					},
+					\"countryEn\": {
+						\"S\": .[2]
+					}
+				}
+			}
+		}
+	)
+} 
+" >split_zones_{#}.json; aws dynamodb batch-write-item   --profile $PROFILE --region $REGION  --endpoint-url=$ENDPOINT --request-items file://split_zones_{#}.json'
+rm -f split_zones_*.json
