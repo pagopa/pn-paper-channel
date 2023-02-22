655ef667a0cf477d636a4e30d1fcd62dccf7991a
pasqualino.cristaudo <>
2023-02-22 09:16:27 +0100

    Fixed some test

--- src/test/java/it/pagopa/pn/paperchannel/mapper/CostMapperTest.java
+++ src/test/java/it/pagopa/pn/paperchannel/mapper/CostMapperTest.java
@@ -21,7 +21,7 @@ class CostMapperTest {
 
     @Test
     void costMappertoInternationalContractTest() {
-        CostDTO response=CostMapper.toCostDTO(getPnPaperCost(ProductTypeEnumDto.RS.getValue()));
+        CostDTO response=CostMapper.toCostDTO(getPnPaperCost(ProductTypeEnumDto.SEMPLICE.getValue()));
         Assertions.assertNotNull(response);
     }
     @Test
