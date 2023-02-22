5bef26e94533f51bb97822fb89f47bddf0d3f55a
elena.ibba <elena.ibba@accenture.com>
2023-02-22 10:49:32 +0100

    check Excel document

---
+++ src/main/java/it/pagopa/pn/paperchannel/utils/costutils/CapProductType.java
@@ -0,0 +1,15 @@
+package it.pagopa.pn.paperchannel.utils.costutils;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CapProductType {
+    private String cap;
+    private String productType;
+}
