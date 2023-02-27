7ed59e9cd468a06b0eadb82c7db9459bec7e6d3f
federica.bellini <ACT3UI0@posteitaliane.it>
2023-02-27 12:27:58 +0100

    junit test QueueListenerServiceImplTest

---
+++ src/test/java/it/pagopa/pn/paperchannel/service/QueueListenerServiceImplTest.java
@@ -0,0 +1,176 @@
+package it.pagopa.pn.paperchannel.service;
+
+import it.pagopa.pn.commons.log.PnAuditLogBuilder;
+import it.pagopa.pn.paperchannel.config.BaseTest;
+import it.pagopa.pn.paperchannel.exception.ExceptionTypeEnum;
+import it.pagopa.pn.paperchannel.exception.PnGenericException;
+import it.pagopa.pn.paperchannel.middleware.db.dao.AddressDAO;
+import it.pagopa.pn.paperchannel.middleware.db.dao.CostDAO;
+import it.pagopa.pn.paperchannel.middleware.db.dao.RequestDeliveryDAO;
+import it.pagopa.pn.paperchannel.middleware.db.entities.PnDeliveryRequest;
+import it.pagopa.pn.paperchannel.middleware.msclient.NationalRegistryClient;
+import it.pagopa.pn.paperchannel.model.PrepareAsyncRequest;
+import it.pagopa.pn.paperchannel.msclient.generated.pnextchannel.v1.dto.SingleStatusUpdateDto;
+import it.pagopa.pn.paperchannel.msclient.generated.pnnationalregistries.v1.dto.AddressOKDto;
+import it.pagopa.pn.paperchannel.msclient.generated.pnnationalregistries.v1.dto.AddressSQSMessageDto;
+import it.pagopa.pn.paperchannel.msclient.generated.pnnationalregistries.v1.dto.AddressSQSMessagePhysicalAddressDto;
+import it.pagopa.pn.paperchannel.service.PaperAsyncService;
+import it.pagopa.pn.paperchannel.service.PaperResultAsyncService;
+import it.pagopa.pn.paperchannel.service.SqsSender;
+import it.pagopa.pn.paperchannel.service.impl.QueueListenerServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import static it.pagopa.pn.paperchannel.exception.ExceptionTypeEnum.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class QueueListenerServiceImplTest extends BaseTest {
+    @MockBean
+    private PaperResultAsyncService paperResultAsyncService;
+    @MockBean
+    private PaperAsyncService paperAsyncService;
+    @MockBean
+    private AddressDAO addressDAO;
+
+    @Autowired
+    @SpyBean
+    PnAuditLogBuilder auditLogBuilder;
+    @MockBean
+    RequestDeliveryDAO requestDeliveryDAO;
+    @MockBean
+    CostDAO costDAO;
+    @MockBean
+    NationalRegistryClient nationalRegistryClient;
+    @MockBean
+    SqsSender sqsSender;
+    @Autowired
+    QueueListenerServiceImpl queueListenerService;
+
+    @BeforeEach
+    public void setUp(){
+        Mockito.when(this.requestDeliveryDAO.getByRequestId(Mockito.anyString())).thenReturn(Mono.just(new PnDeliveryRequest()));
+        Mockito.when(this.requestDeliveryDAO.updateData(Mockito.any())).thenReturn(Mono.just(new PnDeliveryRequest()));
+    }
+
+    @Test
+    void internalListenerTest(){
+        Mockito.when(this.paperAsyncService.prepareAsync(Mockito.any())).thenReturn(Mono.just(new PnDeliveryRequest()));
+        this.queueListenerService.internalListener(new PrepareAsyncRequest(),10);
+    }
+
+    @Test
+    void internalListenerErrorTest(){
+        Mockito.when(this.paperAsyncService.prepareAsync(Mockito.any())).thenReturn(Mono.error(new PnGenericException(ExceptionTypeEnum.PREPARE_ASYNC_LISTENER_EXCEPTION,ExceptionTypeEnum.PREPARE_ASYNC_LISTENER_EXCEPTION.getMessage() )));
+        try{
+            this.queueListenerService.internalListener(new PrepareAsyncRequest(),10);
+        }
+        catch(PnGenericException ex){
+            Assertions.assertEquals(ex.getExceptionType(),PREPARE_ASYNC_LISTENER_EXCEPTION);
+        }
+    }
+
+    @Test
+    void nationalRegistriesResponseListenerUntraceableAddressTest(){
+       try{
+           this.queueListenerService.nationalRegistriesResponseListener(new AddressSQSMessageDto());
+           Assertions.fail("Il metodo non è andato in eccezione");
+       }
+        catch(PnGenericException ex){
+            Assertions.assertEquals(ex.getExceptionType(),UNTRACEABLE_ADDRESS);
+        }
+
+        AddressSQSMessageDto addressSQSMessageDto = new AddressSQSMessageDto();
+        addressSQSMessageDto.setCorrelationId("");
+        try{
+            this.queueListenerService.nationalRegistriesResponseListener(addressSQSMessageDto);
+        }
+        catch(PnGenericException ex){
+            Assertions.assertEquals(ex.getExceptionType(),UNTRACEABLE_ADDRESS);
+        }
+    }
+    @Test
+    void nationalRegistriesResponseListenerDeliveryNotExistsTest (){
+        Mockito.when(this.requestDeliveryDAO.getByCorrelationId(Mockito.anyString())).thenReturn(Mono.empty());
+        AddressSQSMessageDto addressSQSMessageDto = new AddressSQSMessageDto();
+        addressSQSMessageDto.setCorrelationId("1234");
+        try{
+            this.queueListenerService.nationalRegistriesResponseListener(addressSQSMessageDto);
+        }
+        catch(PnGenericException ex){
+            Assertions.assertEquals(ex.getExceptionType(),DELIVERY_REQUEST_NOT_EXIST);
+        }
+    }
+
+    @Test
+    void nationalRegistriesResponseListenerReceivedDeliveryRequestOkLogTest(){
+        PnDeliveryRequest deliveryRequest = new PnDeliveryRequest();
+        deliveryRequest.setIun("1223");
+        deliveryRequest.setRequestId("1234dc");
+        deliveryRequest.setRelatedRequestId("1234abc");
+        deliveryRequest.setCorrelationId("9999");
+        Mockito.when(this.requestDeliveryDAO.getByCorrelationId(Mockito.anyString())).thenReturn(Mono.just(deliveryRequest));
+        AddressSQSMessageDto addressSQSMessageDto = new AddressSQSMessageDto();
+        addressSQSMessageDto.setCorrelationId("1234");
+        addressSQSMessageDto.setError("ok");
+        AddressSQSMessagePhysicalAddressDto addressDto = new AddressSQSMessagePhysicalAddressDto();
+        addressSQSMessageDto.setPhysicalAddress(addressDto);
+        this.queueListenerService.nationalRegistriesResponseListener(addressSQSMessageDto);
+    }
+
+    @Test
+    void nationalRegistriesResponseListenerReceivedDeliveryRequestKoLogTest(){
+        PnDeliveryRequest deliveryRequest = new PnDeliveryRequest();
+        deliveryRequest.setIun("1223");
+        deliveryRequest.setRequestId("1234dc");
+        deliveryRequest.setRelatedRequestId("1234abc");
+        deliveryRequest.setCorrelationId("9999");
+        Mockito.when(this.requestDeliveryDAO.getByCorrelationId(Mockito.anyString())).thenReturn(Mono.just(deliveryRequest));
+        AddressSQSMessageDto addressSQSMessageDto = new AddressSQSMessageDto();
+        addressSQSMessageDto.setCorrelationId("1234");
+        addressSQSMessageDto.setError("");
+        this.queueListenerService.nationalRegistriesResponseListener(addressSQSMessageDto);
+    }
+
+    /*@Test
+    void nationalRegistriesErrorListenerOkTest(){
+        Mockito.when(this.nationalRegistryClient.finderAddress(Mockito.any(), Mockito.any(), Mockito.any())).thenReturn(Mono.just(new AddressOKDto()));
+
+       // this.queueListenerService.externalChannelListener(new SingleStatusUpdateDto(),10);
+    }*/
+
+    /*@Test
+    void nationalRegistriesErrorListenerErrorTest(){
+        Mockito.when(this.paperResultAsyncService.resultAsyncBackground(Mockito.any(), Mockito.any())).thenReturn(Mono.error(new PnGenericException(ExceptionTypeEnum.PREPARE_ASYNC_LISTENER_EXCEPTION,ExceptionTypeEnum.PREPARE_ASYNC_LISTENER_EXCEPTION.getMessage() )));
+        try{
+            this.queueListenerService.externalChannelListener(new SingleStatusUpdateDto(),10);
+        }
+        catch(PnGenericException ex){
+            Assertions.assertEquals(ex.getExceptionType(),PREPARE_ASYNC_LISTENER_EXCEPTION);
+        }
+    }*/
+
+    @Test
+    void externalChannelListenerOkTest(){
+        Mockito.when(this.paperResultAsyncService.resultAsyncBackground(Mockito.any(), Mockito.any())).thenReturn(Mono.just(new PnDeliveryRequest()));
+        this.queueListenerService.externalChannelListener(new SingleStatusUpdateDto(),10);
+    }
+
+    @Test
+    void externalChannelListenerErrorTest(){
+        Mockito.when(this.paperResultAsyncService.resultAsyncBackground(Mockito.any(), Mockito.any())).thenReturn(Mono.error(new PnGenericException(ExceptionTypeEnum.PREPARE_ASYNC_LISTENER_EXCEPTION,ExceptionTypeEnum.PREPARE_ASYNC_LISTENER_EXCEPTION.getMessage() )));
+        try{
+            this.queueListenerService.externalChannelListener(new SingleStatusUpdateDto(),10);
+        }
+        catch(PnGenericException ex){
+            Assertions.assertEquals(ex.getExceptionType(),EXTERNAL_CHANNEL_LISTENER_EXCEPTION);
+        }
+    }
+}
