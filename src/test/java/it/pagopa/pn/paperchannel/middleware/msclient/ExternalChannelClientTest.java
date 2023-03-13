3818ef89ccc65d03277eb73478d4a6e82df74fa4
DIR\luca.ciccarelli <luca.ciccarelli@accenture.com>
2023-03-13 15:40:13 +0100

    junit externalChannelClient

--- src/test/java/it/pagopa/pn/paperchannel/middleware/msclient/ExternalChannelClientTest.java
+++ src/test/java/it/pagopa/pn/paperchannel/middleware/msclient/ExternalChannelClientTest.java
@@ -2,11 +2,13 @@ package it.pagopa.pn.paperchannel.middleware.msclient;
 
 import it.pagopa.pn.paperchannel.config.BaseTest;
 
+import it.pagopa.pn.paperchannel.model.AttachmentInfo;
 import it.pagopa.pn.paperchannel.rest.v1.dto.AnalogAddress;
 import it.pagopa.pn.paperchannel.rest.v1.dto.ProductTypeEnum;
 import it.pagopa.pn.paperchannel.rest.v1.dto.SendRequest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
@@ -15,53 +17,37 @@ import reactor.core.publisher.Mono;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 class ExternalChannelClientTest  extends BaseTest.WithMockServer {
 
     @Autowired
     private ExternalChannelClient externalChannelClient;
 
-    private SendRequest sendRequest;
+    private final SendRequest sendRequest = new SendRequest();
+    private final List<String> attachmentUrls = new ArrayList<>();
+    private final AnalogAddress analogAddress = new AnalogAddress();
 
     @BeforeEach
-    public void setUp() {
-        this.sendRequest = getRequest("abcd12345");
-        sendRequest.setClientRequestTimeStamp(new Date());
-        sendRequest.setProductType(ProductTypeEnum.RIR);
-    }
+    public void setUp() { inizialize();}
 
-    //@Test
+    @Test
     void testOK() {
-        this.sendRequest.getRequestId();
         externalChannelClient.sendEngageRequest(sendRequest, new ArrayList<>()).block();
+        Assertions.assertTrue(true);
     }
 
-    //@Test
+    @Test
     void testBadRequest() {
-        this.sendRequest.getRequestId();
-        externalChannelClient.sendEngageRequest(sendRequest, new ArrayList<>())
-                .onErrorResume(WebClientResponseException.class, ex -> {
-                    Assertions.assertEquals(ex.getStatusCode(), HttpStatus.BAD_REQUEST);
-                    return Mono.empty();
-                }).block();
+        sendRequest.setRequestId(null);
+        WebClientResponseException exception = Assertions.assertThrows(WebClientResponseException.class, ()-> {
+            externalChannelClient.sendEngageRequest(sendRequest, new ArrayList<>()).block();
+        });
+        Assertions.assertEquals(exception.getStatusCode().value(), HttpStatus.BAD_REQUEST.value());
     }
 
-    //@Test
-    void testConflict() {
-        this.sendRequest.getRequestId();
-        externalChannelClient.sendEngageRequest(sendRequest, new ArrayList<>())
-                .onErrorResume(WebClientResponseException.class, ex -> {
-                    Assertions.assertEquals(ex.getStatusCode(), HttpStatus.CONFLICT);
-                    return Mono.empty();
-                }).block();
-    }
-
-    private SendRequest getRequest(String requestId) {
-        SendRequest sendRequest = new SendRequest();
-        List<String> attachmentUrls = new ArrayList<>();
-        AnalogAddress analogAddress = new AnalogAddress();
-        String s = "http://localhost:8080";
-        attachmentUrls.add(s);
+    private void inizialize(){
+        attachmentUrls.add("safestorage://PN_AAR-0002-GR7Z-3UBM-81QT-1QWV");
 
         analogAddress.setAddress("via roma");
         analogAddress.setAddressRow2("via lazio");
@@ -73,15 +59,19 @@ class ExternalChannelClientTest  extends BaseTest.WithMockServer {
         analogAddress.setFullname("Ettore Fieramosca");
         analogAddress.setNameRow2("Ettore");
 
-        sendRequest.setRequestId(requestId);
-        sendRequest.setReceiverFiscalCode("ABCD123AB501");
+        sendRequest.setRequestId("FFPAPERTEST.IUN_FATY-FATY-2023041520230302-101111.RECINDEX_0");
+        sendRequest.setReceiverFiscalCode("PLOMRC01P30L736Y5");
         sendRequest.setProductType(ProductTypeEnum.RIR);
         sendRequest.setReceiverType("PF");
         sendRequest.setPrintType("PT");
         sendRequest.setIun("iun");
+        sendRequest.setRequestPaId("FFPAPERTEST.IUN_FATY-FATY-2023041520230302-101111.RECINDEX_0");
         sendRequest.setAttachmentUrls(attachmentUrls);
         sendRequest.setReceiverAddress(analogAddress);
         sendRequest.setSenderAddress(analogAddress);
-        return sendRequest;
+        sendRequest.setArAddress(analogAddress);
+        sendRequest.setClientRequestTimeStamp(new Date());
+        sendRequest.setProductType(ProductTypeEnum.RIR);
+
     }
 }
