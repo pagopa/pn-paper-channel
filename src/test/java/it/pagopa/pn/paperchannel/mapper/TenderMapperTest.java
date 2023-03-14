4df5c338ebd15a0057ca901dbd3ad0845cd22067
federica.bellini <ACT3UI0@posteitaliane.it>
2023-03-14 14:56:19 +0100

    junit test TenderMapperTest

--- src/test/java/it/pagopa/pn/paperchannel/mapper/TenderMapperTest.java
+++ src/test/java/it/pagopa/pn/paperchannel/mapper/TenderMapperTest.java
@@ -2,8 +2,11 @@ package it.pagopa.pn.paperchannel.mapper;
 
 import it.pagopa.pn.paperchannel.middleware.db.entities.PnTender;
 import it.pagopa.pn.paperchannel.rest.v1.dto.PageableTenderResponseDto;
+import it.pagopa.pn.paperchannel.rest.v1.dto.TenderCreateRequestDTO;
 import it.pagopa.pn.paperchannel.rest.v1.dto.TenderDTO;
+import org.joda.time.Instant;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.data.domain.Pageable;
@@ -11,10 +14,16 @@ import org.springframework.data.domain.Pageable;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 class TenderMapperTest {
 
+    PnTender tender;
+    @BeforeEach
+    void setUp(){
+        this.initialize();
+    }
     @Test
     void exceptionConstructorTest() throws  NoSuchMethodException {
         Constructor<TenderMapper> constructor = TenderMapper.class.getDeclaredConstructor();
@@ -32,10 +41,42 @@ class TenderMapperTest {
         Assertions.assertNotNull(response);
     }
 
-    //@Test
-    void deliveryDriverTenderToDtoTest() {
-        PnTender pnTender = new PnTender();
-        TenderDTO response= TenderMapper.tenderToDto(pnTender);
-        Assertions.assertNotNull(response);
+    @Test
+    void toTenderRequestTest(){
+        TenderCreateRequestDTO requestDTO = new TenderCreateRequestDTO();
+        requestDTO.setCode("code");
+        requestDTO.setName("name");
+        requestDTO.setStartDate(new Date());
+        requestDTO.setEndDate(new Date());
+        PnTender pnTender = TenderMapper.toTenderRequest(requestDTO);
+        Assertions.assertNotNull(pnTender);
+    }
+
+    @Test
+    void toTenderRequestNullCodeTest(){
+        TenderCreateRequestDTO requestDTO = new TenderCreateRequestDTO();
+        requestDTO.setCode(null);
+        requestDTO.setName("name");
+        requestDTO.setStartDate(new Date());
+        requestDTO.setEndDate(new Date());
+        PnTender pnTender = TenderMapper.toTenderRequest(requestDTO);
+        Assertions.assertNotNull(pnTender);
+    }
+
+    @Test
+    void tenderToDtoTest(){
+        TenderDTO tenderDTO = TenderMapper.tenderToDto(tender);
+        Assertions.assertNotNull(tenderDTO);
+    }
+
+    void initialize(){
+        tender = new PnTender();
+        tender.setTenderCode("tenderCode");
+        tender.setDescription("description");
+        tender.setAuthor("author");
+        tender.setStatus(TenderDTO.StatusEnum.CREATED.getValue());
+        tender.setDate(new Date().toInstant());
+        tender.setStartDate(new Date().toInstant());
+        tender.setEndDate(new Date().toInstant());
     }
 }
