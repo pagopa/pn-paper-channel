7b46362601c0da6ab62a9ee0b67308fae04a8daf
Marco Iannaccone <marco.iannaccone@pagopa.it>
2023-03-14 12:18:17 +0100

    PN-4504: closing events unit test

---
+++ src/test/java/it/pagopa/pn/paperchannel/middleware/queue/consumer/handler/AggregatorMessageHandlerTest.java
@@ -0,0 +1,124 @@
+package it.pagopa.pn.paperchannel.middleware.queue.consumer.handler;
+
+import it.pagopa.pn.paperchannel.middleware.db.dao.EventDematDAO;
+import it.pagopa.pn.paperchannel.middleware.db.dao.EventMetaDAO;
+import it.pagopa.pn.paperchannel.middleware.db.entities.PnDeliveryRequest;
+import it.pagopa.pn.paperchannel.middleware.db.entities.PnDiscoveredAddress;
+import it.pagopa.pn.paperchannel.middleware.db.entities.PnEventDemat;
+import it.pagopa.pn.paperchannel.middleware.db.entities.PnEventMeta;
+import it.pagopa.pn.paperchannel.msclient.generated.pnextchannel.v1.dto.PaperProgressStatusEventDto;
+import it.pagopa.pn.paperchannel.rest.v1.dto.SendEvent;
+import it.pagopa.pn.paperchannel.rest.v1.dto.StatusCodeEnum;
+import it.pagopa.pn.paperchannel.service.SqsSender;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.time.temporal.ChronoUnit;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+class AggregatorMessageHandlerTest {
+
+    private AggregatorMessageHandler handler;
+    private SqsSender mockSqsSender;
+    private EventDematDAO mockDematDao;
+    private EventMetaDAO mockMetaDao;
+    @BeforeEach
+    public void init() {
+        mockSqsSender = mock(SqsSender.class);
+        mockMetaDao = mock(EventMetaDAO.class);
+        mockDematDao = mock(EventDematDAO.class);
+
+        handler = new AggregatorMessageHandler(mockSqsSender, mockMetaDao, mockDematDao);
+    }
+
+    @Test
+    void handleAggregatorMessageTest() {
+        // inserimento 1 meta
+        PnEventMeta eventMeta = createPnEventMeta();
+        // inserimento 1 demat
+        PnEventDemat eventDemat = createPnEventDemat();
+
+        // entity and paperRequest
+        OffsetDateTime instant = OffsetDateTime.parse("2023-03-09T14:44:00.000Z");
+        PaperProgressStatusEventDto paperRequest = new PaperProgressStatusEventDto()
+                .requestId("PREPARE_ANALOG_DOMICILE.IUN_MUMR-VQMP-LDNZ-202303-H-1.RECINDEX_0.SENTATTEMPTMADE_0")
+                .statusCode("RECRS002A")
+                .statusDateTime(instant)
+                .clientRequestTimeStamp(instant)
+                .deliveryFailureCause("M02");
+
+        PnDeliveryRequest entity = new PnDeliveryRequest();
+        entity.setRequestId("PREPARE_ANALOG_DOMICILE.IUN_MUMR-VQMP-LDNZ-202303-H-1.RECINDEX_0.SENTATTEMPTMADE_0");
+        entity.setStatusDetail("statusDetail");
+        entity.setStatusCode(StatusCodeEnum.OK.getValue());
+
+        // mock: when
+        // getDeliveryEventMeta
+        when(mockMetaDao.getDeliveryEventMeta(any(String.class), any(String.class))).thenReturn(Mono.just(eventMeta));
+        // findAllByRequestId
+        when(mockDematDao.findAllByRequestId(any(String.class))).thenReturn(Flux.just(eventDemat));
+        // the two deletes
+        when(mockMetaDao.deleteEventMeta(any(String.class), any(String.class))).thenReturn(Mono.just(eventMeta));
+        when(mockDematDao.deleteEventDemat(any(String.class), any(String.class))).thenReturn(Mono.just(eventDemat));
+
+        // assertDoNotThrow with call
+        assertDoesNotThrow(() -> handler.handleMessage(entity, paperRequest).block());
+
+        // check invocations: verify
+        // getDeliveryEventMeta call
+        verify(mockMetaDao, timeout(2000).times(1)).getDeliveryEventMeta(any(String.class), any(String.class));
+        // deleteEventMeta call
+        verify(mockMetaDao, timeout(2000).times(1)).deleteEventMeta(any(String.class), any(String.class));
+        // deleteEventDemat call
+        verify(mockDematDao, timeout(2000).times(1)).deleteEventDemat(any(String.class), any(String.class));
+        // DeliveryPush send via SQS verification
+        verify(mockSqsSender, timeout(2000).times(1)).pushSendEvent(any(SendEvent.class));
+    }
+
+    private PnEventMeta createPnEventMeta() {
+        int ttlOffsetDays = 365;
+        String requestId = "PREPARE_ANALOG_DOMICILE.IUN_MUMR-VQMP-LDNZ-202303-H-1.RECINDEX_0.SENTATTEMPTMADE_0";
+        String statusCode = "RECRS002A";
+
+        PnEventMeta eventMeta = new PnEventMeta();
+        PnDiscoveredAddress address1 = new PnDiscoveredAddress();
+        address1.setAddress("discoveredAddress1");
+        eventMeta.setMetaRequestId("META##" + requestId);
+        eventMeta.setMetaStatusCode("META##" + statusCode);
+        eventMeta.setRequestId(requestId);
+        eventMeta.setStatusCode(statusCode);
+        eventMeta.setDiscoveredAddress(address1);
+        eventMeta.setDeliveryFailureCause("failureCause1");
+        eventMeta.setStatusDateTime(Instant.now());
+        eventMeta.setTtl(Instant.now().plus(ttlOffsetDays, ChronoUnit.DAYS).toEpochMilli());
+
+        return eventMeta;
+    }
+
+    private PnEventDemat createPnEventDemat() {
+        int ttlOffsetDays = 365;
+        String requestId = "PREPARE_ANALOG_DOMICILE.IUN_MUMR-VQMP-LDNZ-202303-H-1.RECINDEX_0.SENTATTEMPTMADE_0";
+        String statusCode = "RECRS002A";
+        String productType = "RS";
+
+        PnEventDemat eventDemat = new PnEventDemat();
+        eventDemat.setDematRequestId("DEMAT##" + requestId);
+        eventDemat.setDocumentTypeStatusCode(productType + "##" + statusCode);
+        eventDemat.setRequestId(requestId);
+        eventDemat.setDocumentType(productType);
+        eventDemat.setStatusCode(statusCode);
+        eventDemat.setDocumentDate(Instant.now().plusSeconds(-5));
+        eventDemat.setStatusDateTime(Instant.now());
+        eventDemat.setUri("uri1");
+        eventDemat.setTtl(Instant.now().plus(ttlOffsetDays, ChronoUnit.DAYS).toEpochMilli());
+
+        return eventDemat;
+    }
+}
