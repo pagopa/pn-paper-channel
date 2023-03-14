22df6eb6c21511118f0733af4ce8c034cbf74c2b
cristopher.moccia <cristopher.moccia@accenture.com>
2023-03-14 14:34:13 +0100

    fixed paper list service test

--- src/test/java/it/pagopa/pn/paperchannel/service/PaperListServiceTest.java
+++ src/test/java/it/pagopa/pn/paperchannel/service/PaperListServiceTest.java
@@ -2,29 +2,45 @@ package it.pagopa.pn.paperchannel.service;
 import it.pagopa.pn.paperchannel.config.BaseTest;
 import it.pagopa.pn.paperchannel.middleware.db.dao.*;
 import it.pagopa.pn.paperchannel.middleware.db.entities.PnCap;
+import it.pagopa.pn.paperchannel.rest.v1.dto.CapResponseDto;
+import it.pagopa.pn.paperchannel.utils.Const;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import reactor.core.publisher.Mono;
 import java.util.ArrayList;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
-public class PaperListServiceTest  extends BaseTest {
+class PaperListServiceTest extends BaseTest {
 
-    @Mock
+    @MockBean
     private CapDAO capDAO;
-
-    @Mock
+    @Autowired
     private PaperListService paperListService;
 
-    //@Test
-    void test(){
+
+    @Test
+    void testGetAllCap(){
         List<PnCap> capResponseDto= new ArrayList<>();
+        PnCap cap = new PnCap();
+        cap.setCap("21034");
+        cap.setCity("Milano");
+        cap.setAuthor(Const.PN_PAPER_CHANNEL);
+
+        capResponseDto.add(cap);
+
         Mockito.when(capDAO.getAllCap(Mockito.any())).thenReturn(Mono.just(capResponseDto));
-        assertNotNull(paperListService.getAllCap("abc" ));
+
+        CapResponseDto response = this.paperListService.getAllCap("").block();
+        assertNotNull(response);
+        assertNotNull(response.getContent());
+        assertEquals(1, response.getContent().size());
+
     }
 
 }
