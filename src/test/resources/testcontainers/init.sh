cfbd6a82115d7e613fd622c968239faaa321b1fc
Simone Rondinella <simone.rondinella@pagopa.it>
2023-03-23 14:01:21 +0100

    modifiche per permettere il funzionamento locale del microservizio

--- src/test/resources/testcontainers/init.sh
+++ src/test/resources/testcontainers/init.sh
@@ -1,12 +1,12 @@
-echo "### CREATE QUEUES ###"
-queues="local-delivery-push-inputs local-delivery-push-safestorage-inputs local-delivery-push-actions local-ext-channels-inputs local-ext-channels-outputs local-delivery-push-actions-done local-ext-channels-elab-res local-user-attributes-actions"
-for qn in  $( echo $queues | tr " " "\n" ) ; do
-    echo creating queue $qn ...
-    aws --profile default --region us-east-1 --endpoint-url http://localstack:4566 \
-        sqs create-queue \
-        --attributes '{"DelaySeconds":"2"}' \
-        --queue-name $qn
-done
+#echo "### CREATE QUEUES ###"
+#queues="local-delivery-push-inputs local-delivery-push-safestorage-inputs local-delivery-push-actions local-ext-channels-inputs local-ext-channels-outputs local-delivery-push-actions-done local-ext-channels-elab-res local-user-attributes-actions"
+#for qn in  $( echo $queues | tr " " "\n" ) ; do
+#    echo creating queue $qn ...
+#    aws --profile default --region us-east-1 --endpoint-url http://localstack:4566 \
+#        sqs create-queue \
+#        --attributes '{"DelaySeconds":"2"}' \
+#        --queue-name $qn
+#done
 
 echo "### CREATE BUCKETS ###"
 buckets="local-doc-bucket local-legal-bucket"
