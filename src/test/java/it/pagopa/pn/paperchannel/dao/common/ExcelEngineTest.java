1e39517a08f32d5d0fa11ab53a33237c38a98bd4
pasqualino.cristaudo <>
2023-03-24 15:39:21 +0100

    Updated Excel Engine draft test

--- src/test/java/it/pagopa/pn/paperchannel/dao/common/ExcelEngineTest.java
+++ src/test/java/it/pagopa/pn/paperchannel/dao/common/ExcelEngineTest.java
@@ -19,45 +19,10 @@ class ExcelEngineTest {
     private XSSFWorkbook workbook;
     private List<XSSFSheet> actualSheets;
 
+
     @BeforeEach
     void setUp(){
         this.initialize();
-    }
-
-    @Test
-    void createNewSheetTest() {
-        String defaultSheet = "Sheet 1";
-        String newSheet = "Sheet 2";
-
-        excelEngine.createNewSheet(newSheet);
-        Assertions.assertNotNull(excelEngine.getCurrentSheet());
-
-        actualSheets.add(workbook.createSheet(defaultSheet));
-        actualSheets.add(workbook.createSheet(newSheet));
-        Assertions.assertIterableEquals(actualSheets, excelEngine.getSheets());
-    }
-
-    @Test
-    void setCurrentSheetTest() {
-        String defaultSheet = "Sheet 1";
-        excelEngine.getWorkbook().removeSheetAt(0);
-        excelEngine.getSheets().remove(excelEngine.getCurrentSheet());
-        excelEngine.setCurrentSheet(defaultSheet);
-        Assertions.assertEquals(0, excelEngine.getSheets().size());
-
-        excelEngine.createNewSheet(defaultSheet);
-        excelEngine.setCurrentSheet(defaultSheet);
-        Assertions.assertEquals(1, excelEngine.getSheets().size());
-
-        String sheet = "Sheet 2";
-        excelEngine.createNewSheet(sheet);
-        excelEngine.setCurrentSheet(sheet);
-        Assertions.assertEquals(sheet, excelEngine.getCurrentSheet().getSheetName());
-
-        String newSheet = "Sheet 3";
-        excelEngine.createNewSheet(newSheet);
-        excelEngine.setCurrentSheet(sheet);
-        Assertions.assertEquals(sheet, excelEngine.getCurrentSheet().getSheetName());
     }
 
     @Test
@@ -66,8 +31,6 @@ class ExcelEngineTest {
         XSSFWorkbook workbook = Mockito.mock(XSSFWorkbook.class);
         Mockito.doNothing().when(workbook).write(os);
         Assertions.assertNotNull(excelEngine.saveOnDisk());
-//        Mockito.verify(workbook, Mockito.times(1));
-//        Mockito.verify(workbook).write(os);
     }
 
     private void initialize() {
