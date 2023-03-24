9e428ac93f7a221977446a29525f68534ead439a
Simone Rondinella <simone.rondinella@pagopa.it>
2023-03-24 16:12:41 +0100

    separata creazione key

--- src/test/resources/testcontainers/init.sh
+++ src/test/resources/testcontainers/init.sh
@@ -1,22 +1,17 @@
-#echo "### CREATE QUEUES ###"
-#queues="local-delivery-push-safestorage-inputs local-delivery-push-actions local-ext-channels-inputs local-ext-channels-outputs local-delivery-push-actions-done local-ext-channels-elab-res local-user-attributes-actions"
-#for qn in  $( echo $queues | tr " " "\n" ) ; do
-#    echo creating queue $qn ...
-#    aws --profile default --region us-east-1 --endpoint-url http://localstack:4566 \
-#        sqs create-queue \
-#        --attributes '{"DelaySeconds":"2"}' \
-#        --queue-name $qn
-#done
+echo "### START KEY KREATION FOR KMS ###"
+aws --profile default --region us-east-1 --endpoint-url=http://localstack:4566 \
+    kms create-key
+echo "### END KEY KREATION FOR KMS ###"
 
-#echo "### CREATE QUEUES FIFO ###"
-#queues_fifo="local-delivery-push-inputs.fifo"
-#for qn in  $( echo $queues_fifo | tr " " "\n" ) ; do
-#    echo creating queue fifo $qn ...
-#    aws --profile default --region us-east-1 --endpoint-url http://localstack:4566 \
-#        sqs create-queue \
-#        --attributes '{"DelaySeconds":"2","FifoQueue": "true","ContentBasedDeduplication": "true"}' \
-#        --queue-name $qn
-#done
+echo "### CREATE QUEUES FIFO ###"
+queues_fifo="local-delivery-push-inputs.fifo"
+for qn in  $( echo $queues_fifo | tr " " "\n" ) ; do
+    echo creating queue fifo $qn ...
+    aws --profile default --region us-east-1 --endpoint-url http://localstack:4566 \
+        sqs create-queue \
+        --attributes '{"DelaySeconds":"2","FifoQueue": "true","ContentBasedDeduplication": "true"}' \
+        --queue-name $qn
+done
 
 echo "### CREATE BUCKETS ###"
 buckets="local-doc-bucket local-legal-bucket"
