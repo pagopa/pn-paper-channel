65908b32642264e25a5139dfba7703a9a27ad8a9
Marco Iannaccone <marco.iannaccone@pagopa.it>
2023-04-03 18:06:33 +0200

    PN-4506: base preparation for integration tests

---
+++ src/test/java/it/pagopa/pn/paperchannel/integrationtests/Paper_RSIT.java
@@ -0,0 +1,23 @@
+package it.pagopa.pn.paperchannel.integrationtests;
+
+import it.pagopa.pn.paperchannel.config.BaseTest;
+import it.pagopa.pn.paperchannel.middleware.db.dao.EventDematDAO;
+import it.pagopa.pn.paperchannel.middleware.db.dao.EventMetaDAO;
+import it.pagopa.pn.paperchannel.service.SqsSender;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+class Paper_RSIT extends BaseTest {
+    @Autowired
+    private EventMetaDAO eventMetaDAO;
+
+    @Autowired
+    private EventDematDAO eventDematDAO;
+
+    @MockBean
+    private SqsSender sqsSender;
+
+    //@BeforeAll
+
+}
