a0e2dfc6ed0e7f626961e5b5a211b0fcc11b181f
EmanueleBonofiglio <emanuele.bonofiglio@grupposcai.it>
2023-04-04 12:06:14 +0200

    PN-4506 mod

--- src/test/java/it/pagopa/pn/paperchannel/integrationtests/Paper_890IT.java
+++ src/test/java/it/pagopa/pn/paperchannel/integrationtests/Paper_890IT.java
@@ -3,8 +3,12 @@ package it.pagopa.pn.paperchannel.integrationtests;
 import it.pagopa.pn.paperchannel.config.BaseTest;
 import it.pagopa.pn.paperchannel.middleware.db.dao.EventDematDAO;
 import it.pagopa.pn.paperchannel.middleware.db.dao.EventMetaDAO;
+import it.pagopa.pn.paperchannel.middleware.queue.consumer.QueueListener;
 import it.pagopa.pn.paperchannel.service.SqsSender;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 class Paper_890IT extends BaseTest {
@@ -14,8 +18,38 @@ class Paper_890IT extends BaseTest {
     @Autowired
     private EventDematDAO eventDematDAO;
 
+    @Autowired
+    private QueueListener queueListener;
+
     @MockBean
     private SqsSender sqsSender;
 
-    //@BeforeAll
+
+    private static String generateObject(String statusCode,String productType){
+        String json = """
+                {
+                     "digitalCourtesy": null,
+                     "digitalLegal": null,
+                     "analogMail": 
+                     {
+                        "requestId": "AKUZ-AWPL-LTPX-20230415",
+                        "registeredLetterCode": null, 
+                        "productType": """+productType+","+"""
+                        "iun": "AKUZ-AWPL-LTPX-20230415",
+                        "statusCode":"""+statusCode+","+"""
+                        "statusDescription": "Mock status",
+                        "statusDateTime": "2023-01-12T14:35:35.135725152Z",
+                        "deliveryFailureCause": null,
+                        "attachments": null,
+                        "discoveredAddress": null,
+                        "clientRequestTimeStamp": "2023-01-12T14:35:35.13572075Z"
+                     }
+                }""";
+        return json;
+    }
+
+    @Test
+    public void test(){
+     System.out.println(generateObject("003","AR"));
+    }
 }
