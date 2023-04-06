0f57def688fa9c424caeab3b9f70a2c44f27665b
Marco Iannaccone <marco.iannaccone@pagopa.it>
2023-04-06 16:49:53 +0200

    PN-4506: basic IT tests organization for AR and RS

--- src/test/java/it/pagopa/pn/paperchannel/integrationtests/Paper_ARIT.java
+++ src/test/java/it/pagopa/pn/paperchannel/integrationtests/Paper_ARIT.java
@@ -4,19 +4,77 @@ import it.pagopa.pn.paperchannel.config.BaseTest;
 import it.pagopa.pn.paperchannel.middleware.db.dao.EventDematDAO;
 import it.pagopa.pn.paperchannel.middleware.db.dao.EventMetaDAO;
 import it.pagopa.pn.paperchannel.service.SqsSender;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.annotation.DirtiesContext;
 
 class Paper_ARIT extends BaseTest {
-    @Autowired
-    private EventMetaDAO eventMetaDAO;
-
-    @Autowired
-    private EventDematDAO eventDematDAO;
 
     @MockBean
     private SqsSender sqsSender;
 
-    //@BeforeAll
+    @DirtiesContext
+    @Test
+    void Test_AR_Delivered__RECRN001A_RECRN001B_RECRN001C(){
+        // meta, demat, final (send to delivery push)
+        // ...
+        //
+        // demat PROGRESS -> send to delivery push
+    }
 
+    @DirtiesContext
+    @Test
+    void Test_AR_NotDelivered__RECRN002A_RECRN002B_RECRN002C(){
+        // meta, demat, final (send to delivery push)
+        // ...
+        // deliveryFailureCause
+        //
+        // demat PROGRESS -> send to delivery push
+    }
+
+    @DirtiesContext
+    @Test
+    void Test_AR_AbsoluteUntraceability__RECRN002D_RECRN002E_RECRN002F(){
+        // meta, demat, final (send to delivery push)
+        // ...
+        // deliveryFailureCause
+        // optional discoveredAddress
+        //
+        // demat PROGRESS -> send to delivery push
+    }
+
+    @DirtiesContext
+    @Test
+    void Test_AR_DeliveredToStorage__RECRN003A_RECRN003B_RECRN003C(){
+        // meta, demat, final (send to delivery push)
+        // ...
+        //
+        // demat PROGRESS -> send to delivery push
+    }
+
+    @DirtiesContext
+    @Test
+    void Test_AR_RefusedToStorage__RECRN004A_RECRN004B_RECRN004C(){
+        // meta, demat, final (send to delivery push)
+        // ...
+        //
+        // demat PROGRESS -> send to delivery push
+    }
+
+    @DirtiesContext
+    @Test
+    void Test_AR_CompletedStorage__RECRN005A_RECRN005B_RECRN005C(){
+        // meta, demat, final (send to delivery push)
+        // ...
+        //
+        // demat PROGRESS -> send to delivery push
+    }
+
+    @DirtiesContext
+    @Test
+    void Test_AR_TheftLossDeterioration__RECRN006__RetryPC(){
+        // retry paper channel
+        // ...
+    }
 }
--- src/test/java/it/pagopa/pn/paperchannel/integrationtests/Paper_RSIT.java
+++ src/test/java/it/pagopa/pn/paperchannel/integrationtests/Paper_RSIT.java
@@ -5,19 +5,72 @@ import it.pagopa.pn.paperchannel.middleware.db.dao.EventDematDAO;
 import it.pagopa.pn.paperchannel.middleware.db.dao.EventMetaDAO;
 import it.pagopa.pn.paperchannel.service.SqsSender;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.annotation.DirtiesContext;
 
 class Paper_RSIT extends BaseTest {
-    @Autowired
-    private EventMetaDAO eventMetaDAO;
-
-    @Autowired
-    private EventDematDAO eventDematDAO;
 
     @MockBean
     private SqsSender sqsSender;
 
-    //@BeforeAll
+    @DirtiesContext
+    @Test
+    void Test_RS_Delivered__RECRS001C(){
+        // final only -> send to delivery push
+        // ...
+    }
 
+    @DirtiesContext
+    @Test
+    void Test_RS_NotDelivered__RECRS002A_RECRS002B_RECRS002C(){
+        // meta, demat, final (send to delivery push)
+        // ...
+        // deliveryFailureCause
+        //
+        // demat PROGRESS -> send to delivery push
+    }
+
+    @DirtiesContext
+    @Test
+    void Test_RS_AbsoluteUntraceability__RECRS002D_RECRS002E_RECRS002F(){
+        // meta, demat, final (send to delivery push)
+        // ...
+        // deliveryFailureCause
+        //
+        // demat PROGRESS -> send to delivery push
+    }
+
+    @DirtiesContext
+    @Test
+    void Test_RS_DeliveredToStorage__RECRS003C(){
+        // final only -> send to delivery push
+        // ...
+    }
+
+    @DirtiesContext
+    @Test
+    void Test_RS_RefusedToStorage__RECRS004A_RECRS004B_RECRS004C(){
+        // meta, demat, final (send to delivery push)
+        // ...
+        //
+        // demat PROGRESS -> send to delivery push
+    }
+
+    @DirtiesContext
+    @Test
+    void Test_RS_CompletedStorage__RECRS005A_RECRS005B_RECRS005C(){
+        // meta, demat, final (send to delivery push)
+        // ...
+        //
+        // demat PROGRESS -> send to delivery push
+    }
+
+    @DirtiesContext
+    @Test
+    void Test_RS_TheftLossDeterioration__RECRS006__RetryPC(){
+        // retry paper channel
+        // ...
+    }
 }
