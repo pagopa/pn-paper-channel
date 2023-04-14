790b0cf64fc094c2fe439bdcb560fe77bec885bc
pasqualino.cristaudo <>
2023-04-14 11:04:17 +0200

    Added logs in case of failure of the comparison between request and entity

--- src/main/java/it/pagopa/pn/paperchannel/validator/PrepareRequestValidator.java
+++ src/main/java/it/pagopa/pn/paperchannel/validator/PrepareRequestValidator.java
@@ -5,6 +5,7 @@ import it.pagopa.pn.paperchannel.middleware.db.entities.PnAttachmentInfo;
 import it.pagopa.pn.paperchannel.middleware.db.entities.PnDeliveryRequest;
 import it.pagopa.pn.paperchannel.rest.v1.dto.PrepareRequest;
 import it.pagopa.pn.paperchannel.utils.Utility;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
 
@@ -15,6 +16,8 @@ import java.util.stream.Collectors;
 import static it.pagopa.pn.paperchannel.exception.ExceptionTypeEnum.DIFFERENT_DATA_REQUEST;
 import static it.pagopa.pn.paperchannel.mapper.AddressMapper.fromAnalogToAddress;
 
+
+@Slf4j
 public class PrepareRequestValidator {
 
     private PrepareRequestValidator(){
@@ -26,26 +29,32 @@ public class PrepareRequestValidator {
 
         if (!StringUtils.equals(prepareRequest.getRequestId(), pnDeliveryEntity.getRequestId())) {
             errors.add("RequestId");
+            log.debug("Comparison between request and entity failed, different data: RequestId");
         }
 
         if (!StringUtils.equals(prepareRequest.getIun(), pnDeliveryEntity.getIun())) {
             errors.add("Iun");
+            log.debug("Comparison between request and entity failed, different data: Iun");
         }
 
         if (!StringUtils.equals(Utility.convertToHash(prepareRequest.getReceiverFiscalCode()), pnDeliveryEntity.getHashedFiscalCode())) {
             errors.add("FiscalCode");
+            log.debug("Comparison between request and entity failed, different data: FiscalCode");
         }
 
         if (!StringUtils.equals(prepareRequest.getProposalProductType().getValue(), pnDeliveryEntity.getProposalProductType())) {
             errors.add("ProductType");
+            log.debug("Comparison between request and entity failed, different data: ProductType");
         }
 
         if (!StringUtils.equals(prepareRequest.getReceiverType(), pnDeliveryEntity.getReceiverType())) {
             errors.add("ReceiverType");
+            log.debug("Comparison between request and entity failed, different data: ReceiverType");
         }
 
         if (!StringUtils.equals(prepareRequest.getPrintType(), (pnDeliveryEntity.getPrintType()))) {
-            errors.add("printType");
+            errors.add("PrintType");
+            log.debug("Comparison between request and entity failed, different data: PrintType");
         }
 
         if (pnDeliveryEntity.getAttachments() != null) {
@@ -53,19 +62,21 @@ public class PrepareRequestValidator {
                     .map(PnAttachmentInfo::getFileKey).collect(Collectors.toList());
             if (!AttachmentValidator.checkBetweenLists(prepareRequest.getAttachmentUrls(), fromDb)) {
                 errors.add("Attachments");
+                log.debug("Comparison between request and entity failed, different data: Attachments");
             }
         }
-
 
 
         if (firstAttempt) {
             if (prepareRequest.getReceiverAddress() != null) {
                 if (!StringUtils.equals(fromAnalogToAddress(prepareRequest.getReceiverAddress()).convertToHash(), (pnDeliveryEntity.getAddressHash()))) {
                     errors.add("Address");
+                    log.debug("Comparison between request and entity failed, different data: Address");
                 }
 
             } else {
                 errors.add("Address");
+                log.debug("Comparison between request and entity failed, different data: Address");
             }
         }
 
