c785590879af1e47897579f0710a3de22d49fa03
cristopher.moccia <cristopher.moccia@accenture.com>
2023-05-04 11:04:25 +0200

    added log into prepare controller

--- src/main/java/it/pagopa/pn/paperchannel/rest/v1/PaperMessagesRestV1Controller.java
+++ src/main/java/it/pagopa/pn/paperchannel/rest/v1/PaperMessagesRestV1Controller.java
@@ -3,12 +3,14 @@ package it.pagopa.pn.paperchannel.rest.v1;
 import it.pagopa.pn.paperchannel.rest.v1.api.PaperMessagesApi;
 import it.pagopa.pn.paperchannel.rest.v1.dto.*;
 import it.pagopa.pn.paperchannel.service.PaperMessagesService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @RestController
 public class PaperMessagesRestV1Controller implements PaperMessagesApi {
 
@@ -17,9 +19,15 @@ public class PaperMessagesRestV1Controller implements PaperMessagesApi {
 
     @Override
     public Mono<ResponseEntity<PaperChannelUpdate>> sendPaperPrepareRequest(String requestId, Mono<PrepareRequest> prepareRequest, ServerWebExchange exchange) {
-       return prepareRequest.flatMap(request -> paperMessagesService.preparePaperSync(requestId, request))
+       return prepareRequest
+               .doOnNext(request -> {
+                   log.debug("Delivery Request of prepare flow");
+                   log.debug(request.getReceiverAddress().toString());
+               })
+               .flatMap(request -> paperMessagesService.preparePaperSync(requestId, request))
                 .map(ResponseEntity::ok);
     }
+
     @Override
     public Mono<ResponseEntity<PrepareEvent>> retrievePaperPrepareRequest(String requestId, ServerWebExchange exchange) {
         return paperMessagesService.retrievePaperPrepareRequest(requestId).map(ResponseEntity::ok);
