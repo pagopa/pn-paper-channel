f2b763f0503bd47654da3a634e5c01ca8ad1f1ea
federica.bellini <ACT3UI0@posteitaliane.it>
2023-05-19 16:19:11 +0200

    added AOP logs, fixed pom with new dependency

---
+++ src/main/java/it/pagopa/pn/paperchannel/utils/LoggerComponent.java
@@ -0,0 +1,62 @@
+package it.pagopa.pn.paperchannel.utils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.CodeSignature;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Aspect
+@Component
+public class LoggerComponent {
+    private static final Logger logger = LoggerFactory.getLogger(LoggerComponent.class);
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    @Pointcut("within(it.pagopa.pn.paperchannel.rest.v1..*) " +
+            "&& @annotation(org.springframework.web.bind.annotation.RequestMapping)")
+    public void pointcut() {
+    }
+
+    @Before("pointcut()")
+    public void logMethod(JoinPoint joinPoint) {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        logger.debug("==> path(s): {}, method(s): {}, arguments: {} ", null, null , signature);
+    }
+
+    @AfterReturning(pointcut = "pointcut()", returning = "entity")
+    public void logMethodAfter(JoinPoint joinPoint, ResponseEntity<?> entity) {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        logger.info("==> path(s): {}, method(s): {}, arguments: {} ", null, null , signature);
+    }
+
+    private Map<String, Object> getParameters(JoinPoint joinPoint) {
+        CodeSignature signature = (CodeSignature) joinPoint.getSignature();
+
+        HashMap<String, Object> map = new HashMap<>();
+
+        String[] parameterNames = signature.getParameterNames();
+
+        for (int i = 0; i < parameterNames.length; i++) {
+            map.put(parameterNames[i], joinPoint.getArgs()[i]);
+        }
+
+        return map;
+    }
+
+}
