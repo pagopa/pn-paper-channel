4de0ec6343e8d40e34e2e36891ef315b1b0b73a6
cristopher.moccia <cristopher.moccia@accenture.com>
2023-06-06 09:29:49 +0200

    added flow < 10 days

--- src/main/java/it/pagopa/pn/paperchannel/middleware/queue/consumer/handler/RECRN00XCMessageHandler.java
+++ src/main/java/it/pagopa/pn/paperchannel/middleware/queue/consumer/handler/RECRN00XCMessageHandler.java
@@ -1,33 +1,94 @@
 package it.pagopa.pn.paperchannel.middleware.queue.consumer.handler;
 
+import it.pagopa.pn.paperchannel.mapper.common.BaseMapperImpl;
 import it.pagopa.pn.paperchannel.middleware.db.dao.EventMetaDAO;
 import it.pagopa.pn.paperchannel.middleware.db.entities.PnDeliveryRequest;
+import it.pagopa.pn.paperchannel.middleware.db.entities.PnDiscoveredAddress;
 import it.pagopa.pn.paperchannel.middleware.db.entities.PnEventMeta;
+import it.pagopa.pn.paperchannel.middleware.queue.consumer.MetaDematCleaner;
+import it.pagopa.pn.paperchannel.middleware.queue.model.PNRN012Wrapper;
+import it.pagopa.pn.paperchannel.msclient.generated.pnextchannel.v1.dto.DiscoveredAddressDto;
 import it.pagopa.pn.paperchannel.msclient.generated.pnextchannel.v1.dto.PaperProgressStatusEventDto;
+import it.pagopa.pn.paperchannel.rest.v1.dto.StatusCodeEnum;
 import it.pagopa.pn.paperchannel.service.SqsSender;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
 import static it.pagopa.pn.paperchannel.utils.MetaDematUtils.*;
 
 @Slf4j
 public class RECRN00XCMessageHandler extends SendToDeliveryPushHandler {
     private final EventMetaDAO eventMetaDAO;
+    private final MetaDematCleaner metaDematCleaner;
 
-    public RECRN00XCMessageHandler(SqsSender sqsSender, EventMetaDAO eventMetaDAO) {
+    public RECRN00XCMessageHandler(SqsSender sqsSender, EventMetaDAO eventMetaDAO, MetaDematCleaner metaDematCleaner) {
         super(sqsSender);
         this.eventMetaDAO = eventMetaDAO;
+        this.metaDematCleaner = metaDematCleaner;
     }
 
 
     @Override
     public Mono<Void> handleMessage(PnDeliveryRequest entity, PaperProgressStatusEventDto paperRequest) {
+        final String status = paperRequest.getStatusCode()
+                .substring(0,paperRequest.getStatusCode().length()-1)
+                .concat("A");
 
-        return this.eventMetaDAO.getDeliveryEventMeta(buildMetaRequestId(entity.getRequestId()), buildMetaStatusCode(RECRN011_STATUS_CODE))
+        final String metaRequestId = buildMetaRequestId(entity.getRequestId());
+
+        return this.eventMetaDAO.getDeliveryEventMeta(metaRequestId, buildMetaStatusCode(RECRN011_STATUS_CODE))
                 .switchIfEmpty(Mono.defer(() -> {
-                    log.warn("[{}] Missing EventMeta for {}", paperRequest.getRequestId(), paperRequest);
-                    return Mono.just(new PnEventMeta());
+                    log.warn("[{}] Missing EventMeta RECRN011 for {}", paperRequest.getRequestId(), paperRequest);
+                    return Mono.empty();
                 }))
-                .then();
+                .zipWhen(n011 ->
+                        this.eventMetaDAO.getDeliveryEventMeta(metaRequestId, buildMetaStatusCode(status))
+                                .switchIfEmpty(Mono.defer(() -> {
+                                    log.warn("[{}] Missing EventMeta RECRN011 for {}", paperRequest.getRequestId(), paperRequest);
+                                    return Mono.empty();
+                                }))
+                )
+                .flatMap(recrn011AndRecrn00X -> {
+                    PnEventMeta eventrecrn011 = recrn011AndRecrn00X.getT1();
+                    PnEventMeta eventrecrn00X = recrn011AndRecrn00X.getT2();
+
+                    if (isThenGratherOrEquals10Days(eventrecrn00X.getStatusDateTime(), eventrecrn011.getStatusDateTime())) {
+                        PNRN012Wrapper pnrn012Wrapper = PNRN012Wrapper.buildPNRN012Wrapper(entity, paperRequest, eventrecrn011.getStatusDateTime().plus(10, ChronoUnit.DAYS));
+                        var pnrn012PaperRequest = pnrn012Wrapper.getPaperProgressStatusEventDtoPNRN012();
+                        var pnrn012DeliveryRequest = pnrn012Wrapper.getPnDeliveryRequestPNRN012();
+                        pnrn012DeliveryRequest.setStatusDetail(StatusCodeEnum.PROGRESS.getValue());
+                        return super.handleMessage(pnrn012DeliveryRequest, pnrn012PaperRequest);
+                    }
+
+                    return Mono.just(enrichEvent(paperRequest, eventrecrn00X))
+                            .flatMap(enrichedRequest -> super.handleMessage(entity, enrichedRequest))
+                            .then(metaDematCleaner.clean(paperRequest.getRequestId()));
+                });
     }
+
+    private PaperProgressStatusEventDto enrichEvent(PaperProgressStatusEventDto paperRequest, PnEventMeta pnEventMeta) {
+
+        if (pnEventMeta.getDiscoveredAddress() != null) {
+            DiscoveredAddressDto discoveredAddressDto = new BaseMapperImpl<>(PnDiscoveredAddress.class, DiscoveredAddressDto.class)
+                                                                .toDTO(pnEventMeta.getDiscoveredAddress());
+            paperRequest.setDiscoveredAddress(discoveredAddressDto);
+
+            log.info("[{}] Discovered Address in EventMeta for {}", paperRequest.getRequestId(), pnEventMeta);
+        }
+        paperRequest.setDeliveryFailureCause(pnEventMeta.getDeliveryFailureCause());
+
+        return paperRequest;
+    }
+
+
+
+    private boolean isThenGratherOrEquals10Days(Instant recrn00XTimestamp, Instant recrn011Timestamp){
+        long duration = Duration.between(recrn011Timestamp, recrn00XTimestamp).toDays();
+        return duration >= 10;
+    }
+
 }
