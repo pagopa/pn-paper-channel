d8ad17150aa7a40efbcaccacce84d59766f977ce
ariannazafarana <112564573+ariannazafarana@users.noreply.github.com>
2023-06-30 14:28:36 +0200

    remove logs

--- src/main/java/it/pagopa/pn/paperchannel/middleware/db/dao/PaperRequestErrorDAO.java
+++ src/main/java/it/pagopa/pn/paperchannel/middleware/db/dao/PaperRequestErrorDAO.java
@@ -1,8 +1,6 @@
 package it.pagopa.pn.paperchannel.middleware.db.dao;
 
-import it.pagopa.pn.paperchannel.generated.openapi.server.v1.dto.InfoDownloadDTO;
 import it.pagopa.pn.paperchannel.middleware.db.entities.PnRequestError;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
