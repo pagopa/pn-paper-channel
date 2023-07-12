f6eba3ae947f33124770b6a7beb27769758450ee
cristopher.moccia <cristopher.moccia@accenture.com>
2023-07-12 15:44:28 +0200

    additional fixes on not retryable flow

--- src/main/java/it/pagopa/pn/paperchannel/middleware/queue/consumer/handler/NotRetryableErrorMessageHandler.java
+++ src/main/java/it/pagopa/pn/paperchannel/middleware/queue/consumer/handler/NotRetryableErrorMessageHandler.java
@@ -8,6 +8,7 @@ import it.pagopa.pn.paperchannel.service.SqsSender;
 import it.pagopa.pn.paperchannel.utils.PnLogAudit;
 import reactor.core.publisher.Mono;
 
+
 public class NotRetryableErrorMessageHandler extends SendToDeliveryPushHandler {
 
     private final PaperRequestErrorDAO paperRequestErrorDAO;
@@ -22,14 +23,16 @@ public class NotRetryableErrorMessageHandler extends SendToDeliveryPushHandler {
         PnAuditLogBuilder auditLogBuilder = new PnAuditLogBuilder();
         PnLogAudit pnLogAudit = new PnLogAudit(auditLogBuilder);
         pnLogAudit.addsBeforeDiscard(entity.getIun(), String.format("requestId = %s finish retry to External Channel", entity.getRequestId()));
-
         return super.handleMessage(entity, paperRequest)
-                .doOnSuccess(pnRequestError -> {
-                    paperRequestErrorDAO.created(entity.getRequestId(), entity.getStatusCode(), entity.getStatusDetail());
-                    pnLogAudit.addsSuccessDiscard(entity.getIun(), String.format("requestId = %s finish retry to External Channel", entity.getRequestId()));
-                })
+                .then(Mono.defer(() ->
+                    paperRequestErrorDAO.created(entity.getRequestId(), entity.getStatusCode(), entity.getStatusDetail())
+                        .doOnSuccess(requestError ->
+                            pnLogAudit.addsSuccessDiscard(entity.getIun(), String.format("requestId = %s finish retry to External Channel", entity.getRequestId()))
+                        )
+                ))
                 .doOnError(throwable -> pnLogAudit.addsFailDiscard(entity.getIun(), String.format("requestId = %s finish retry to External Channel", entity.getRequestId())))
                 .then();
+
     }
 
 }
