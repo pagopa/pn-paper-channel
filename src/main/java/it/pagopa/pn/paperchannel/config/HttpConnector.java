eeb258b8244f3038e80a51f7a32df502b92f2a20
vincenzo-racca-pa <vincenzo.racca@pagopa.it>
2023-07-24 13:28:09 +0200

    download PDF in chunks

--- src/main/java/it/pagopa/pn/paperchannel/config/HttpConnector.java
+++ src/main/java/it/pagopa/pn/paperchannel/config/HttpConnector.java
@@ -1,14 +1,18 @@
 package it.pagopa.pn.paperchannel.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.pdfbox.io.IOUtils;
+import org.apache.pdfbox.io.RandomAccessBuffer;
+import org.apache.pdfbox.pdfparser.PDFParser;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferUtils;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
 
 @Slf4j
 public class HttpConnector {
@@ -18,29 +22,40 @@ public class HttpConnector {
     }
 
     public static Mono<PDDocument> downloadFile(String url) {
-        log.info("Url to download: {}", url);
+        // DataBuffer and DataBufferUtils to stream our download in chunks so that the whole file doesn't get loaded
+        // into memory
+        Flux<DataBuffer> dataBufferFlux = WebClient.create()
+                .get()
+                .uri(url)
+                .accept(MediaType.APPLICATION_PDF)
+                .retrieve()
+                .bodyToFlux(DataBuffer.class);
+
+        return DataBufferUtils.join(dataBufferFlux)
+                .map(dataBuffer -> {
+                    byte[] bytes = new byte[dataBuffer.readableByteCount()];
+                    dataBuffer.read(bytes);
+                    DataBufferUtils.release(dataBuffer);
+                    return bytes;
+                })
+                .map(HttpConnector::buildPDDocument);
+    }
+
+
+    private static PDDocument buildPDDocument(byte[] pdfByteArray) {
+        PDDocument pdDocument = null;
         try {
-            return WebClient
-                    .builder()
-                    .codecs(codecs ->
-                            codecs.defaultCodecs()
-                                    .maxInMemorySize(-1)
-                    )
-                    .build()
-                    .get()
-                    .uri(new URI(url))
-                    .accept(MediaType.APPLICATION_PDF)
-                    .retrieve()
-                    .bodyToMono(byte[].class)
-                    .flatMap(bytes -> {
-                        try {
-                            return Mono.just(PDDocument.load(bytes));
-                        } catch (IOException e) {
-                            return Mono.error(e);
-                        }
-                    });
-        } catch (URISyntaxException e) {
-            return Mono.error(e);
+            PDFParser parser = new PDFParser(new RandomAccessBuffer(pdfByteArray));
+            parser.parse();
+            pdDocument = parser.getPDDocument();
+            return pdDocument;
         }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        finally {
+            IOUtils.closeQuietly(pdDocument);
+        }
+
     }
 }
