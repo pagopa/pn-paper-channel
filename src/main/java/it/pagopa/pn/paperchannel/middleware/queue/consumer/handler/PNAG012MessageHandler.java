a7577335df79daf2688cec9b3a4151ac1bcfdb24
vincenzo-racca-pa <vincenzo.racca@pagopa.it>
2023-12-04 11:06:52 +0100

    PN-8911 add comment

--- src/main/java/it/pagopa/pn/paperchannel/middleware/queue/consumer/handler/PNAG012MessageHandler.java
+++ src/main/java/it/pagopa/pn/paperchannel/middleware/queue/consumer/handler/PNAG012MessageHandler.java
@@ -93,6 +93,6 @@ public class PNAG012MessageHandler extends SaveDematMessageHandler {
         pnLogAudit.addsBeforeReceive(pnag012DeliveryRequest.getIun(), String.format("prepare requestId = %s Response from external-channel", pnag012DeliveryRequest.getRequestId()));
         return eventMetaDAO.putIfAbsent(pnEventMetaPNAG012)
                 .doOnNext(pnEventMetaRECAG012Updated -> logSuccessAuditLog(pnag012PaperRequest, pnag012DeliveryRequest, pnLogAudit))
-                .map(pnEventMeta -> pnag012Wrapper);
+                .map(pnEventMeta -> pnag012Wrapper); //blocco il flusso se la putIfAbsent non inserisce a DB
     }
 }
