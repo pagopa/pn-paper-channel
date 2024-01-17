797066b202e0f78e14d7d5dd3b885b241ac19744
Christian La Riccia <153509595+clariccia@users.noreply.github.com>
2024-01-17 12:45:22 +0100

    Bug/pn-9526 (#419)
    * PN-9526 fix numberPages null
    
    * feat(pn-9526): add timeout config for f24 client
    
    * docs(pn-9526): update F24ClientReadTimeoutMillis parameter description
    
    * test(pn-9526): update F24ServiceImplTest cases
    
    * feat(pn-9526): add retry max attempts for F24 client
    
    * feat(pn-9526): change internal queue visibility timeout to 60 seconds
    
    ---------
    
    Co-authored-by: vincenzo-racca-pa <vincenzo.racca@pagopa.it>

---
+++ src/main/java/it/pagopa/pn/paperchannel/config/F24ClientConfig.java
@@ -0,0 +1,37 @@
+package it.pagopa.pn.paperchannel.config;
+
+import it.pagopa.pn.commons.pnclients.CommonBaseClient;
+import it.pagopa.pn.paperchannel.generated.openapi.msclient.pnf24.v1.ApiClient;
+import it.pagopa.pn.paperchannel.generated.openapi.msclient.pnf24.v1.api.F24ControllerApi;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+@Slf4j
+public class F24ClientConfig extends CommonBaseClient {
+
+    @Bean
+    public F24ControllerApi getF24ControllerAPI(PnPaperChannelConfig pnPaperChannelConfig){
+
+        ApiClient newApiClient = new ApiClient(super.initWebClient(ApiClient.buildWebClientBuilder()));
+        newApiClient.setBasePath(pnPaperChannelConfig.getClientF24Basepath());
+        return new F24ControllerApi(newApiClient);
+    }
+
+    @Autowired
+    @Override
+    public void setReadTimeoutMillis(@Value("${pn.paper-channel.f24client.timeout-millis}") int readTimeoutMillis) {
+        super.setReadTimeoutMillis(readTimeoutMillis);
+    }
+
+    @Autowired
+    @Override
+    public void setRetryMaxAttempts(@Value("${pn.paper-channel.f24client.retry-max-attempts}") int retryMaxAttempts) {
+        super.setRetryMaxAttempts(retryMaxAttempts);
+    }
+}
