5453d937abbaec62cf9863a6124f6306efb3e390
Giacomo Vallorani <giacomo.vallorani@pagopa.it>
2024-06-28 11:27:20 +0200

    refactor(PN-11739): use Instant for feedback status date/time fields

--- src/main/java/it/pagopa/pn/paperchannel/utils/FeedbackStatus.java
+++ src/main/java/it/pagopa/pn/paperchannel/utils/FeedbackStatus.java
@@ -1,14 +1,15 @@
 package it.pagopa.pn.paperchannel.utils;
 
 import java.io.Serializable;
+import java.time.Instant;
 
 /**
  * This class groups and centralizes attributes related to feedback.
  */
 public record FeedbackStatus(String oldFeedbackStatusCode,
                              String newFeedbackStatusCode,
-                             String oldFeedbackStatusDateTime,
-                             String newFeedbackStatusDateTime,
+                             Instant oldFeedbackStatusDateTime,
+                             Instant newFeedbackStatusDateTime,
                              String oldFeedbackDeliveryFailureCause,
                              String newFeedbackDeliveryFailureCause)
                             implements Serializable {}
