2150d0646df2718c35040f4a568a5da6c89989c7
Giacomo Vallorani <giacomo.vallorani@pagopa.it>
2024-06-28 09:47:55 +0200

    refactor(PN-11739): remove @NonNull annotations from FeedbackStatus record

--- src/main/java/it/pagopa/pn/paperchannel/utils/FeedbackStatus.java
+++ src/main/java/it/pagopa/pn/paperchannel/utils/FeedbackStatus.java
@@ -1,13 +1,11 @@
 package it.pagopa.pn.paperchannel.utils;
 
-import lombok.NonNull;
-
 /**
  * This class groups and centralizes attributes related to feedback.
  *
  * @param oldFeedbackDeliveryFailureCause There may not have been a delivery failure cause, so it can be null.
  */
-public record FeedbackStatus(@NonNull String oldFeedbackStatusCode, @NonNull String newFeedbackStatusCode,
-                             @NonNull String oldFeedbackStatusDateTime, @NonNull String newFeedbackStatusDateTime,
+public record FeedbackStatus(String oldFeedbackStatusCode, String newFeedbackStatusCode,
+                             String oldFeedbackStatusDateTime, String newFeedbackStatusDateTime,
                              String oldFeedbackDeliveryFailureCause, String newFeedbackDeliveryFailureCause) {
 }
