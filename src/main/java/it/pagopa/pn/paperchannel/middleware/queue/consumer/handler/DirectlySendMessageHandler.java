89b0411f0b7059004eaef541838f0fc134387e62
Giacomo Vallorani <giacomo.vallorani@pagopa.it>
2024-08-06 16:28:11 +0200

    refactor(PN-12287): delete DirectlySendMessageHandler.java

--- src/main/java/it/pagopa/pn/paperchannel/middleware/queue/consumer/handler/DirectlySendMessageHandler.java
+++
@@ -1,9 +0,0 @@
-package it.pagopa.pn.paperchannel.middleware.queue.consumer.handler;
-
-import lombok.experimental.SuperBuilder;
-
-/**
- * Possiamo eliminare questa classe rendendo {@link SendToDeliveryPushHandler} non astratta?
- * */
-@SuperBuilder
-public class DirectlySendMessageHandler extends SendToDeliveryPushHandler { }
