8190ce5a9dbcdf2cb51aaa6ab6266fe3daea3f3b
ariannazafarana <112564573+ariannazafarana@users.noreply.github.com>
2024-09-02 15:50:57 +0200

    adds api to calculate cost

---
+++ src/main/java/it/pagopa/pn/paperchannel/rest/v1/PaperCalculatorRestV1Controller.java
@@ -0,0 +1,20 @@
+package it.pagopa.pn.paperchannel.rest.v1;
+
+
+import it.pagopa.pn.paperchannel.generated.openapi.server.v1.api.PaperCalculatorApi;
+import it.pagopa.pn.paperchannel.generated.openapi.server.v1.dto.ShipmentCalculateRequest;
+import it.pagopa.pn.paperchannel.generated.openapi.server.v1.dto.ShipmentCalculateResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class PaperCalculatorRestV1Controller implements PaperCalculatorApi {
+
+    @Override
+    public Mono<ResponseEntity<ShipmentCalculateResponse>> calculateCost(String tenderId,
+        Mono<ShipmentCalculateRequest> shipmentCalculateRequest, ServerWebExchange exchange) {
+        return PaperCalculatorApi.super.calculateCost(tenderId, shipmentCalculateRequest, exchange);
+    }
+}
