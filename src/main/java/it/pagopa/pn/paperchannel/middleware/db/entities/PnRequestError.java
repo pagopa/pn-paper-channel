3c848ea8a2496a048328a536c8e23130174ca61c
Giacomo Vallorani <giacomo.vallorani@pagopa.it>
2025-02-10 12:14:27 +0100

    feat(PN-13579): update paperRequestError Structure

--- src/main/java/it/pagopa/pn/paperchannel/middleware/db/entities/PnRequestError.java
+++ src/main/java/it/pagopa/pn/paperchannel/middleware/db/entities/PnRequestError.java
@@ -1,6 +1,7 @@
 package it.pagopa.pn.paperchannel.middleware.db.entities;
 
-
+import it.pagopa.pn.paperchannel.model.RequestErrorCategoryEnum;
+import it.pagopa.pn.paperchannel.model.RequestErrorCauseEnum;
 import it.pagopa.pn.paperchannel.utils.Const;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,6 +22,8 @@ public class PnRequestError {
     public static final String COL_REQUEST_ID = "requestId";
     public static final String COL_CREATED = "created";
     public static final String COL_PA_ID = "paId";
+    public static final String COL_CATEGORY = "category";
+    public static final String COL_CAUSE = "cause";
     public static final String COL_ERROR_TYPE = "error";
     public static final String COL_FLOW_THROW = "flowThrow";
     public static final String COL_AUTHOR = "author";
@@ -28,16 +31,33 @@ public class PnRequestError {
 
     /* Indexes */
     public static final String AUTHOR_INDEX = "author-index";
+    public static final String CATEGORY_INDEX = "category-index";
 
-
-    @Getter(onMethod = @__({@DynamoDbPartitionKey, @DynamoDbAttribute(COL_REQUEST_ID)}))
+    @Getter(onMethod = @__({
+            @DynamoDbPartitionKey,
+            @DynamoDbAttribute(COL_REQUEST_ID)}))
     public String requestId;
 
-    @Getter(onMethod = @__({@DynamoDbSortKey,@DynamoDbSecondarySortKey(indexNames = AUTHOR_INDEX), @DynamoDbAttribute(COL_CREATED)}))
+    @Getter(onMethod = @__({@DynamoDbSortKey,
+            @DynamoDbSecondarySortKey(indexNames = AUTHOR_INDEX),
+            @DynamoDbAttribute(COL_CREATED)}))
     public Instant created;
 
-    @Getter(onMethod = @__({@DynamoDbSecondaryPartitionKey(indexNames = AUTHOR_INDEX), @DynamoDbAttribute(COL_AUTHOR)}))
+    @Getter(onMethod = @__({
+            @DynamoDbSecondaryPartitionKey(indexNames = AUTHOR_INDEX),
+            @DynamoDbAttribute(COL_AUTHOR)}))
     public String author;
+
+    @Getter(onMethod = @__({
+            @DynamoDbSecondaryPartitionKey(indexNames = CATEGORY_INDEX),
+            @DynamoDbAttribute(COL_CATEGORY)}))
+    public String category;
+
+    // Format: <CAUSE>##<TIMESTAMP>
+    @Getter(onMethod = @__({
+            @DynamoDbSecondarySortKey(indexNames = CATEGORY_INDEX),
+            @DynamoDbAttribute(COL_CAUSE)}))
+    public String cause;
 
     @Getter(onMethod = @__({@DynamoDbAttribute(COL_PA_ID)}))
     public String paId;
@@ -63,6 +83,8 @@ public class PnRequestError {
         private String error;
         private String flowThrow;
         private String geokey;
+        private RequestErrorCategoryEnum category = RequestErrorCategoryEnum.UNKNOWN;
+        private RequestErrorCauseEnum cause = RequestErrorCauseEnum.UNKNOWN;
 
         // Private constructor
         private PnRequestErrorBuilder() {}
@@ -92,17 +114,30 @@ public class PnRequestError {
             return this;
         }
 
+        public PnRequestErrorBuilder category(RequestErrorCategoryEnum category) {
+            this.category = category;
+            return this;
+        }
+
+        public PnRequestErrorBuilder cause(RequestErrorCauseEnum cause) {
+            this.cause = cause;
+            return this;
+        }
+
         public PnRequestError build() {
+            var timestamp = Instant.now();
             PnRequestError pnRequestError = new PnRequestError();
 
             pnRequestError.setRequestId(this.requestId);
             pnRequestError.setPaId(this.paId);
             pnRequestError.setError(this.error);
             pnRequestError.setFlowThrow(this.flowThrow);
-            pnRequestError.setGeokey(geokey);
+            pnRequestError.setGeokey(this.geokey);
+            pnRequestError.setCategory(this.category.getValue());
+            pnRequestError.setCause(this.cause.getValue() + "##" + timestamp);
 
             /* Auto-generated constant field values */
-            pnRequestError.setCreated(Instant.now());
+            pnRequestError.setCreated(timestamp);
             pnRequestError.setAuthor(Const.PN_PAPER_CHANNEL);
 
             return pnRequestError;
