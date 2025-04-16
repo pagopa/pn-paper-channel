def0325f760aea14acfab745be764b369ecbe569
Giacomo Vallorani <giacomo.vallorani@pagopa.it>
2025-04-16 19:04:12 +0200

    feat(PN-14591): add EnableOldFlowRECRN004C param

--- src/main/java/it/pagopa/pn/paperchannel/config/PnPaperChannelConfig.java
+++ src/main/java/it/pagopa/pn/paperchannel/config/PnPaperChannelConfig.java
@@ -81,6 +81,7 @@ public class PnPaperChannelConfig {
     private boolean prepareTwoPhases;
     private Duration compiutaGiacenzaArDuration;
     private boolean enableTruncatedDateForRefinementCheck;
+    private boolean enableOldFlowRECRN004C;
 
     /**
      * Per l'errore PNADDR001 flusso NR: True se il failureDetailCode D01 deve essere mandato a delivery push (specificando anche l'indirizzo),
