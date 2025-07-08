928172a71f282f9d1066a1426733a89c821f2759
Giacomo Vallorani <giacomo.vallorani@pagopa.it>
2025-07-08 10:48:30 +0200

    feat(PN-15435): add OCR queues and config

--- src/main/java/it/pagopa/pn/paperchannel/config/PnPaperChannelConfig.java
+++ src/main/java/it/pagopa/pn/paperchannel/config/PnPaperChannelConfig.java
@@ -46,6 +46,7 @@ public class PnPaperChannelConfig {
     private String queuePaperchannelToDelayer;
     private String queueDelayerToPaperchannel;
     private String queueInternal;
+    private String queueUrlOcrInputs;
     private Integer attemptSafeStorage;
     private Integer attemptQueueSafeStorage;
     private Integer attemptQueueExternalChannel;
@@ -81,6 +82,7 @@ public class PnPaperChannelConfig {
     private boolean prepareTwoPhases;
     private Duration compiutaGiacenzaArDuration;
     private boolean enableTruncatedDateForRefinementCheck;
+    private boolean enableOcr;
 
     /**
      * Per l'errore PNADDR001 flusso NR: True se il failureDetailCode D01 deve essere mandato a delivery push (specificando anche l'indirizzo),
