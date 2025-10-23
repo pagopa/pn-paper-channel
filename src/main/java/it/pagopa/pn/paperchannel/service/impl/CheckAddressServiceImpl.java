911480d36ce56c578011a13ef642254b1c3aa3a4
Stefano Rughetti <stefano.rughetti@emeal.nttdata.com>
2025-10-23 09:54:50 +0200

    [PN-16853] Feature: Refactor buildAddressResponse

--- src/main/java/it/pagopa/pn/paperchannel/service/impl/CheckAddressServiceImpl.java
+++ src/main/java/it/pagopa/pn/paperchannel/service/impl/CheckAddressServiceImpl.java
@@ -1,20 +1,15 @@
 package it.pagopa.pn.paperchannel.service.impl;
 
-import it.pagopa.pn.paperchannel.exception.PnGenericException;
 import it.pagopa.pn.paperchannel.generated.openapi.server.v1.dto.CheckAddressResponse;
 import it.pagopa.pn.paperchannel.middleware.db.dao.AddressDAO;
-import it.pagopa.pn.paperchannel.middleware.db.entities.PnAddress;
 import it.pagopa.pn.paperchannel.service.CheckAddressService;
 import lombok.CustomLog;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 import java.time.Instant;
-import java.util.Objects;
 
-import static it.pagopa.pn.paperchannel.exception.ExceptionTypeEnum.DELIVERY_REQUEST_NOT_EXIST;
 import static it.pagopa.pn.paperchannel.utils.AddressTypeEnum.RECEIVER_ADDRESS;
 
 @CustomLog
@@ -28,22 +23,17 @@ public class CheckAddressServiceImpl implements CheckAddressService {
     public Mono<CheckAddressResponse> checkAddressRequest(String requestId){
         log.info("Finding address for requestId {}", requestId);
         return addressDAO.findByRequestId(requestId, RECEIVER_ADDRESS)
-                .flatMap(address -> buildAddressFoundResponse(address, requestId))
-                .switchIfEmpty(buildAddressNotFoundResponse(requestId));
+                .flatMap(address -> buildAddressResponse(requestId, true, address.getTtl()))
+                .switchIfEmpty(buildAddressResponse(requestId, false, null));
     }
 
-    private Mono<CheckAddressResponse> buildAddressFoundResponse(PnAddress address, String requestId) {
+    private Mono<CheckAddressResponse> buildAddressResponse(String requestId, boolean found, Long ttl) {
         CheckAddressResponse response = new CheckAddressResponse();
-        response.setFound(true);
+        response.setFound(found);
         response.setRequestId(requestId);
-        response.setEndValidity(Instant.ofEpochSecond(address.getTtl()));
-        return Mono.just(response);
-    }
-
-    private Mono<CheckAddressResponse> buildAddressNotFoundResponse(String requestId) {
-        CheckAddressResponse response = new CheckAddressResponse();
-        response.setFound(false);
-        response.setRequestId(requestId);
+        if (found) {
+            response.setEndValidity(Instant.ofEpochSecond(ttl));
+        }
         return Mono.just(response);
     }
 
