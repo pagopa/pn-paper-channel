2c74316110bb907f9f2f87cb25e9c17fbd5d6789
CapolongoDa <daniele.capolongo@emeal.nttdata.com>
2025-11-05 17:17:50 +0100

    [PN-16219] feat: added test

--- src/test/java/it/pagopa/pn/paperchannel/service/impl/PreparePhaseOneAsyncServiceImplTest.java
+++ src/test/java/it/pagopa/pn/paperchannel/service/impl/PreparePhaseOneAsyncServiceImplTest.java
@@ -1,6 +1,8 @@
 package it.pagopa.pn.paperchannel.service.impl;
 
 import it.pagopa.pn.paperchannel.config.PnPaperChannelConfig;
+import it.pagopa.pn.paperchannel.exception.CheckAddressFlowException;
+import it.pagopa.pn.paperchannel.exception.PnAddressFlowException;
 import it.pagopa.pn.paperchannel.exception.PnGenericException;
 import it.pagopa.pn.paperchannel.exception.PnUntracebleException;
 import it.pagopa.pn.paperchannel.generated.openapi.server.v1.dto.FailureDetailCodeEnum;
@@ -10,26 +12,29 @@ import it.pagopa.pn.paperchannel.middleware.db.dao.AddressDAO;
 import it.pagopa.pn.paperchannel.middleware.db.dao.PaperChannelDeliveryDriverDAO;
 import it.pagopa.pn.paperchannel.middleware.db.dao.PaperRequestErrorDAO;
 import it.pagopa.pn.paperchannel.middleware.db.dao.RequestDeliveryDAO;
-import it.pagopa.pn.paperchannel.middleware.db.entities.PaperChannelDeliveryDriver;
-import it.pagopa.pn.paperchannel.middleware.db.entities.PnAddress;
-import it.pagopa.pn.paperchannel.middleware.db.entities.PnAttachmentInfo;
-import it.pagopa.pn.paperchannel.middleware.db.entities.PnDeliveryRequest;
+import it.pagopa.pn.paperchannel.middleware.db.entities.*;
 import it.pagopa.pn.paperchannel.model.*;
 import it.pagopa.pn.paperchannel.service.*;
 import it.pagopa.pn.paperchannel.utils.AddressTypeEnum;
 import it.pagopa.pn.paperchannel.utils.PaperCalculatorUtils;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
+import static it.pagopa.pn.paperchannel.exception.ExceptionTypeEnum.*;
+import static it.pagopa.pn.paperchannel.exception.ExceptionTypeEnum.ADDRESS_NOT_EXIST;
 import static it.pagopa.pn.paperchannel.utils.Const.RACCOMANDATA_SEMPLICE;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.mockito.ArgumentMatchers.*;
@@ -175,6 +180,10 @@ class PreparePhaseOneAsyncServiceImplTest {
         String statusDetail = statusDeliveryEnum.getDetail();
         KOReason koReason = new KOReason(FailureDetailCodeEnum.D00, null);
 
+        ArgumentCaptor<String> descriptionCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> statusCodeCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> statusDetailCaptor = ArgumentCaptor.forClass(String.class);
+
         when(this.requestDeliveryDAO.getByRequestId(any(), anyBoolean()))
                 .thenReturn(Mono.just(deliveryRequest));
 
@@ -200,7 +209,240 @@ class PreparePhaseOneAsyncServiceImplTest {
 
         verify(this.prepareFlowStarter, times(1)).pushResultPrepareEvent(eq(deliveryRequest), isNull(), eq("clientId"), eq(StatusCodeEnum.KO), eq(koReason));
         verify(prepareFlowStarter, never()).pushPreparePhaseOneOutput(any(), any(), any());
-        verify(requestDeliveryDAO,times(1)).updateStatus(eq(deliveryRequest.getRequestId()), eq(statusCode), eq(statusDescription), eq(statusDetail), any());
+        verify(requestDeliveryDAO,times(1)).updateStatus(eq(deliveryRequest.getRequestId()), statusCodeCaptor.capture(), descriptionCaptor.capture(), statusDetailCaptor.capture(), any());
+
+        Assertions.assertEquals(statusCode, statusCodeCaptor.getValue());
+        Assertions.assertEquals(statusDescription, descriptionCaptor.getValue());
+        Assertions.assertEquals(statusDetail, statusDetailCaptor.getValue());
+    }
+
+    @Test
+    void preparePhaseOneAsyncFilterAttachmentTest() {
+        var requestId = "PREPARE_ANALOG_DOMICILE.IUN_GJWA-HMEK-RGUJ-202307-H-1.RECINDEX_0.ATTEMPT_0";
+        var iun = "GJWA-HMEK-RGUJ-202307-H-1";
+        var deliveryRequest =  getDeliveryRequest(requestId, iun);
+        PrepareNormalizeAddressEvent event = PrepareNormalizeAddressEvent.builder()
+                .requestId(requestId)
+                .iun(iun)
+                .attempt(0)
+                .build();
+
+        var address = getAddress();
+
+        ArgumentCaptor<String> descriptionCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> statusCodeCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> statusDetailCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<PnRequestError> itemErrorCaptor = ArgumentCaptor.forClass(PnRequestError.class);
+
+        StatusDeliveryEnum statusDeliveryEnum = StatusDeliveryEnum.PAPER_CHANNEL_ASYNC_ERROR;
+        String statusCode = statusDeliveryEnum.getCode();
+        String statusDescription = statusCode + " - " + statusDeliveryEnum.getDescription();
+        String statusDetail = statusDeliveryEnum.getDetail();
+
+        PnRequestError pnRequestError = new PnRequestError();
+        pnRequestError.setError("");
+        pnRequestError.setGeokey("geokey");
+        pnRequestError.setFlowThrow("PREPARE_PHASE_ONE_ASYNC_DEFAULT");
+        pnRequestError.setCause("UNKNOWN##"+ Instant.now().toString());
+        pnRequestError.setCategory("UNKNOWN");
+        pnRequestError.setAuthor("PN-PAPER-CHANNEL");
+        pnRequestError.setRequestId("FATY-FATY-2023041520230302");
+        pnRequestError.setCreated(Instant.now());
+
+        RuntimeException runtimeException = new RuntimeException("Generic error");
+
+        PnAddress addressEntity = AddressMapper.toEntity(address, deliveryRequest.getRequestId(), AddressTypeEnum.RECEIVER_ADDRESS, config);
+
+        var cost = new PnPaperChannelCostDTO();
+        cost.setTenderId("TENDER_ID");
+        cost.setDeliveryDriverId("DRIVER_ID");
+
+        var driver = new PaperChannelDeliveryDriver();
+        driver.setUnifiedDeliveryDriver("UNIFIED_DRIVER");
+
+        when(requestDeliveryDAO.getByRequestId(requestId, false)).thenReturn(Mono.just(deliveryRequest));
+        when(paperAddressService.getCorrectAddress(deliveryRequest, null, 0)).thenReturn(Mono.just(address));
+        when(addressDAO.create(any(PnAddress.class))).thenReturn(Mono.just(addressEntity));
+        when(attachmentsConfigService.filterAttachmentsToSend(deliveryRequest, deliveryRequest.getAttachments(), addressEntity)).thenReturn(Mono.error(runtimeException));
+        when(requestDeliveryDAO.updateStatus(any(), any(), any(), any(), any())).thenReturn(Mono.empty());
+        when(paperRequestErrorDAO.created(any())).thenReturn(Mono.just(pnRequestError));
+
+        StepVerifier.create(this.preparePhaseOneAsyncService.preparePhaseOneAsync(event))
+                .expectErrorMatches(ex -> {
+                    assertInstanceOf(RuntimeException.class, ex);
+                    return true;
+                }).verify();
+
+        verify(this.prepareFlowStarter, never()).pushResultPrepareEvent(eq(deliveryRequest), isNull(), eq("clientId"), eq(StatusCodeEnum.KO), isNull());
+        verify(paperRequestErrorDAO, times(1)).created(itemErrorCaptor.capture());
+        verify(requestDeliveryDAO, times(1)).updateStatus(eq(requestId), statusCodeCaptor.capture(), descriptionCaptor.capture(), statusDetailCaptor.capture(), any());
+
+        Assertions.assertEquals(statusDescription, descriptionCaptor.getValue());
+        Assertions.assertEquals(statusCode, statusCodeCaptor.getValue());
+        Assertions.assertEquals(statusDetail, statusDetailCaptor.getValue());
+    }
+
+    @Test
+    void preparePhaseOneAsyncTestCheckAddressError(){
+        PnDeliveryRequest deliveryRequest = getDeliveryRequest("FATY-FATY-2023041520230302", "FATY-FATY-2023041520230302-101111");
+        String requestId = deliveryRequest.getRequestId();
+        ArgumentCaptor<String> descriptionCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> statusCodeCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> statusDetailCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<PnRequestError> itemErrorCaptor = ArgumentCaptor.forClass(PnRequestError.class);
+
+        StatusDeliveryEnum statusDeliveryEnum = StatusDeliveryEnum.PAPER_CHANNEL_ASYNC_ERROR;
+        String statusCode = statusDeliveryEnum.getCode();
+        String statusDescription = statusCode + " - " + statusDeliveryEnum.getDescription();
+        String statusDetail = statusDeliveryEnum.getDetail();
+
+        PnRequestError pnRequestError = new PnRequestError();
+        pnRequestError.setError("");
+        pnRequestError.setGeokey("geokey");
+        pnRequestError.setFlowThrow("CHECK_ADDRESS_FLOW");
+        pnRequestError.setCause("UNKNOWN##"+ Instant.now().toString());
+        pnRequestError.setCategory("UNKNOWN");
+        pnRequestError.setAuthor("PN-PAPER-CHANNEL");
+        pnRequestError.setRequestId("FATY-FATY-2023041520230302");
+        pnRequestError.setCreated(Instant.now());
+
+        CheckAddressFlowException checkAddressFlowException = new CheckAddressFlowException(new Throwable(), "geokey");
+
+        when(this.requestDeliveryDAO.getByRequestId(any(), anyBoolean()))
+                .thenReturn(Mono.just(deliveryRequest));
+
+        when(this.paperAddressService.getCorrectAddress(any(), any(), anyInt()))
+                .thenReturn(Mono.error(checkAddressFlowException));
+        when(paperRequestErrorDAO.created(any())).thenReturn(Mono.just(pnRequestError));
+        when(requestDeliveryDAO.updateStatus(eq(requestId), eq(statusCode), eq(statusDescription), eq(statusDetail), any())).thenReturn(Mono.empty());
+
+        PrepareNormalizeAddressEvent event = PrepareNormalizeAddressEvent.builder()
+                .requestId("FATY-FATY-2023041520230302")
+                .clientId("clientId")
+                .iun("FATY-FATY-2023041520230302-101111")
+                .attempt(0)
+                .build();
+
+        StepVerifier.create(this.preparePhaseOneAsyncService.preparePhaseOneAsync(event))
+                .expectErrorMatches(ex -> {
+                    assertInstanceOf(CheckAddressFlowException.class, ex);
+                    return true;
+                }).verify();
+
+        verify(this.prepareFlowStarter, never()).pushResultPrepareEvent(eq(deliveryRequest), isNull(), eq("clientId"), eq(StatusCodeEnum.KO), isNull());
+        verify(paperRequestErrorDAO, times(1)).created(itemErrorCaptor.capture());
+        verify(requestDeliveryDAO, times(1)).updateStatus(eq(requestId), statusCodeCaptor.capture(), descriptionCaptor.capture(), statusDetailCaptor.capture(), any());
+
+        Assertions.assertEquals(statusDescription, descriptionCaptor.getValue());
+        Assertions.assertEquals(statusCode, statusCodeCaptor.getValue());
+        Assertions.assertEquals(statusDetail, statusDetailCaptor.getValue());
+    }
+
+    @Test
+    void preparePhaseOneAsyncTestErrorPnAddressFlowPostmanFlow(){
+        PnDeliveryRequest deliveryRequest = getDeliveryRequest("FATY-FATY-2023041520230302", "FATY-FATY-2023041520230302-101111");
+
+        when(this.requestDeliveryDAO.getByRequestId(any(), anyBoolean()))
+                .thenReturn(Mono.just(deliveryRequest));
+
+        when(this.paperAddressService.getCorrectAddress(any(), any(), anyInt()))
+                .thenReturn(Mono.error(new PnAddressFlowException(ATTEMPT_ADDRESS_NATIONAL_REGISTRY)));
+
+        PrepareNormalizeAddressEvent event = PrepareNormalizeAddressEvent.builder()
+                .requestId("FATY-FATY-2023041520230302")
+                .clientId("clientId")
+                .iun("FATY-FATY-2023041520230302-101111")
+                .attempt(0)
+                .build();
+
+        StepVerifier.create(this.preparePhaseOneAsyncService.preparePhaseOneAsync(event))
+                .expectErrorMatches(ex -> {
+                    assertInstanceOf(PnAddressFlowException.class, ex);
+                    return true;
+                }).verify();
+
+        verify(this.prepareFlowStarter, never()).pushResultPrepareEvent(eq(deliveryRequest), isNull(), eq("clientId"), eq(StatusCodeEnum.KO), isNull());
+        verify(prepareFlowStarter, never()).pushPreparePhaseOneOutput(any(), any(), any());
+        verify(requestDeliveryDAO,never()).updateStatus(eq(deliveryRequest.getRequestId()), any(), any(), any(), any());
+    }
+    @Test
+    void preparePhaseOneAsyncTestPnAddresManagerError(){
+        PnDeliveryRequest deliveryRequest = getDeliveryRequest("FATY-FATY-2023041520230302", "FATY-FATY-2023041520230302-101111");
+
+        when(this.requestDeliveryDAO.getByRequestId(any(), anyBoolean()))
+                .thenReturn(Mono.just(deliveryRequest));
+
+        when(this.paperAddressService.getCorrectAddress(any(), any(), anyInt()))
+                .thenReturn(Mono.error(new PnAddressFlowException(ADDRESS_MANAGER_ERROR)));
+
+        PrepareNormalizeAddressEvent event = PrepareNormalizeAddressEvent.builder()
+                .requestId("FATY-FATY-2023041520230302")
+                .clientId("clientId")
+                .iun("FATY-FATY-2023041520230302-101111")
+                .attempt(0)
+                .build();
+
+        StepVerifier.create(this.preparePhaseOneAsyncService.preparePhaseOneAsync(event))
+                .expectErrorMatches(ex -> {
+                    assertInstanceOf(PnAddressFlowException.class, ex);
+                    return true;
+                }).verify();
+
+        verify(this.prepareFlowStarter, never()).pushResultPrepareEvent(eq(deliveryRequest), isNull(), eq("clientId"), eq(StatusCodeEnum.KO), isNull());
+        verify(prepareFlowStarter, never()).pushPreparePhaseOneOutput(any(), any(), any());
+        verify(requestDeliveryDAO,never()).updateStatus(eq(deliveryRequest.getRequestId()), any(), any(), any(), any());
+    }
+    @Test
+    void preparePhaseOneAsyncTestGetRequestError(){
+        PnDeliveryRequest deliveryRequest = getDeliveryRequest("FATY-FATY-2023041520230302", "FATY-FATY-2023041520230302-101111");
+        String requestId = deliveryRequest.getRequestId();
+        ArgumentCaptor<String> descriptionCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> statusCodeCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> statusDetailCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<PnRequestError> itemErrorCaptor = ArgumentCaptor.forClass(PnRequestError.class);
+
+        StatusDeliveryEnum statusDeliveryEnum = StatusDeliveryEnum.PAPER_CHANNEL_ASYNC_ERROR;
+        String statusCode = statusDeliveryEnum.getCode();
+        String statusDescription = statusCode + " - " + statusDeliveryEnum.getDescription();
+        String statusDetail = statusDeliveryEnum.getDetail();
+
+        PnRequestError pnRequestError = new PnRequestError();
+        pnRequestError.setError("");
+        pnRequestError.setGeokey("geokey");
+        pnRequestError.setFlowThrow("PPREPARE_PHASE_ONE_ASYNC_DEFAULT");
+        pnRequestError.setCause("UNKNOWN##"+ Instant.now().toString());
+        pnRequestError.setCategory("UNKNOWN");
+        pnRequestError.setAuthor("PN-PAPER-CHANNEL");
+        pnRequestError.setRequestId("FATY-FATY-2023041520230302");
+        pnRequestError.setCreated(Instant.now());
+
+        RuntimeException runtimeException = new RuntimeException("Generic error");
+
+        when(this.requestDeliveryDAO.getByRequestId(any(), anyBoolean()))
+                .thenReturn(Mono.error(runtimeException));
+        when(paperRequestErrorDAO.created(any())).thenReturn(Mono.just(pnRequestError));
+        when(requestDeliveryDAO.updateStatus(eq(requestId), eq(statusCode), eq(statusDescription), eq(statusDetail), any())).thenReturn(Mono.empty());
+
+        PrepareNormalizeAddressEvent event = PrepareNormalizeAddressEvent.builder()
+                .requestId("FATY-FATY-2023041520230302")
+                .clientId("clientId")
+                .iun("FATY-FATY-2023041520230302-101111")
+                .attempt(0)
+                .build();
+
+        StepVerifier.create(this.preparePhaseOneAsyncService.preparePhaseOneAsync(event))
+                .expectErrorMatches(ex -> {
+                    assertInstanceOf(RuntimeException.class, ex);
+                    return true;
+                }).verify();
+
+        verify(this.prepareFlowStarter, never()).pushResultPrepareEvent(eq(deliveryRequest), isNull(), eq("clientId"), eq(StatusCodeEnum.KO), isNull());
+        verify(paperRequestErrorDAO, times(1)).created(itemErrorCaptor.capture());
+        verify(requestDeliveryDAO, times(1)).updateStatus(eq(requestId), statusCodeCaptor.capture(), descriptionCaptor.capture(), statusDetailCaptor.capture(), any());
+
+        Assertions.assertEquals(statusDescription, descriptionCaptor.getValue());
+        Assertions.assertEquals(statusCode, statusCodeCaptor.getValue());
+        Assertions.assertEquals(statusDetail, statusDetailCaptor.getValue());
     }
 
     @Test
