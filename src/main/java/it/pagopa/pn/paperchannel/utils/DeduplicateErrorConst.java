a9ab46f9a1b1623a92aab361f191ca98a5bf5421
Vincenzo Racca <vincenzo.racca@pagopa.it>
2025-11-10 16:26:03 +0100

    PN-16696 - introdotto errore PNADDR003

--- src/main/java/it/pagopa/pn/paperchannel/utils/DeduplicateErrorConst.java
+++ src/main/java/it/pagopa/pn/paperchannel/utils/DeduplicateErrorConst.java
@@ -4,6 +4,7 @@ public class DeduplicateErrorConst {
 
     public static final String PNADDR001 = "PNADDR001";
     public static final String PNADDR002 = "PNADDR002";
+    public static final String PNADDR003 = "PNADDR003";
     public static final String PNADDR999 = "PNADDR999";
 
     private DeduplicateErrorConst() {}
