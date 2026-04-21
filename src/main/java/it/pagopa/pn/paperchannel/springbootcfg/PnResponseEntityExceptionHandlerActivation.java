1d52fe24065e22e6ea93687662a6d855cc52fdd1
Vincenzo Racca <113343826+vincenzo-racca-pa@users.noreply.github.com>
2026-04-21 14:50:56 +0200

    feat: Added PnResponseEntityExceptionHandlerActivation (ex handler of SEND) (#728)
    * fix 400 Bad request
    
    * Added junit test for handleServerWebInputException
    
    * fix 400 Bad request
    
    * fix sonar issues
    
    ---------
    
    Co-authored-by: federica.moschese <federica.moschese@emeal.nttdata.com>

--- src/main/java/it/pagopa/pn/paperchannel/springbootcfg/PnResponseEntityExceptionHandlerActivation.java
+++ src/main/java/it/pagopa/pn/paperchannel/springbootcfg/PnResponseEntityExceptionHandlerActivation.java
@@ -1,13 +1,19 @@
-package it.pagopa.pn.paperchannel.exception;
+package it.pagopa.pn.paperchannel.springbootcfg;
 
-import com.fasterxml.jackson.databind.JsonMappingException;
 
+import it.pagopa.pn.commons.exceptions.ExceptionHelper;
+import it.pagopa.pn.commons.exceptions.PnResponseEntityExceptionHandler;
 import it.pagopa.pn.commons.utils.MDCUtils;
+import it.pagopa.pn.paperchannel.exception.PnExcelValidatorException;
+import it.pagopa.pn.paperchannel.exception.PnGenericException;
+import it.pagopa.pn.paperchannel.exception.PnInputValidatorException;
+import it.pagopa.pn.paperchannel.exception.PnPaperEventException;
 import it.pagopa.pn.paperchannel.generated.openapi.server.v1.dto.PaperEvent;
 import it.pagopa.pn.paperchannel.generated.openapi.server.v1.dto.Problem;
 import it.pagopa.pn.paperchannel.generated.openapi.server.v1.dto.ProblemError;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -15,17 +21,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import reactor.core.publisher.Mono;
 
 import java.time.Instant;
-import java.util.Date;
 import java.util.stream.Collectors;
 
-@Slf4j
 @ControllerAdvice
-public class RestExceptionHandler {
+@Import(ExceptionHelper.class)
+@Slf4j
+public class PnResponseEntityExceptionHandlerActivation extends PnResponseEntityExceptionHandler {
 
-    @ExceptionHandler(JsonMappingException.class)
-    public void handle(JsonMappingException e) {
-        log.error("Returning HTTP 400 Bad Request {}", e.getMessage());
+    public PnResponseEntityExceptionHandlerActivation(ExceptionHelper exceptionHelper) {
+        super(exceptionHelper);
     }
+
 
     @ExceptionHandler(PnGenericException.class)
     public Mono<ResponseEntity<Problem>> handleResponseEntityException(final PnGenericException exception){
@@ -57,7 +63,7 @@ public class RestExceptionHandler {
             item.setDetail("Trovato valore differente a db");
             item.setElement(error);
             return item;
-        }).collect(Collectors.toList()));
+        }).toList());
         settingTraceId(problem);
         problem.setTimestamp(Instant.now());
 
@@ -78,13 +84,12 @@ public class RestExceptionHandler {
             item.setDetail(error.getCol().toString());
             item.setElement(error.getMessage());
             return item;
-        }).collect(Collectors.toList()));
+        }).toList());
         settingTraceId(problem);
         problem.setTimestamp(Instant.now());
 
         return Mono.just(ResponseEntity.status(problem.getStatus()).body(problem));
     }
-
 
     private void settingTraceId(Problem problem){
         try {
