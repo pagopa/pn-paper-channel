fbcc6dbdc45f747d188c5dea8e5789d2dc41e87a
Vincenzo Racca <vincenzo.racca@pagopa.it>
2026-04-21 14:30:37 +0200

    fix 400 Bad request

--- src/main/java/it/pagopa/pn/paperchannel/springbootcfg/PnResponseEntityExceptionHandlerActivation.java
+++ src/main/java/it/pagopa/pn/paperchannel/springbootcfg/PnResponseEntityExceptionHandlerActivation.java
@@ -1,33 +1,37 @@
-package it.pagopa.pn.paperchannel.exception;
+package it.pagopa.pn.paperchannel.springbootcfg;
 
-import com.fasterxml.jackson.databind.JsonMappingException;
 
+import it.pagopa.pn.commons.exceptions.ExceptionHelper;
+import it.pagopa.pn.commons.exceptions.PnResponseEntityExceptionHandler;
 import it.pagopa.pn.commons.utils.MDCUtils;
+import it.pagopa.pn.paperchannel.exception.PnExcelValidatorException;
+import it.pagopa.pn.paperchannel.exception.PnGenericException;
+import it.pagopa.pn.paperchannel.exception.PnInputValidatorException;
+import it.pagopa.pn.paperchannel.exception.PnPaperEventException;
 import it.pagopa.pn.paperchannel.generated.openapi.server.v1.dto.PaperEvent;
 import it.pagopa.pn.paperchannel.generated.openapi.server.v1.dto.Problem;
 import it.pagopa.pn.paperchannel.generated.openapi.server.v1.dto.ProblemError;
 import lombok.extern.slf4j.Slf4j;
-import org.jetbrains.annotations.NotNull;
 import org.slf4j.MDC;
-import org.springframework.http.*;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.reactive.result.method.annotation.ResponseEntityExceptionHandler;
-import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.server.ServerWebInputException;
 import reactor.core.publisher.Mono;
 
 import java.time.Instant;
 import java.util.stream.Collectors;
 
-@Slf4j
 @ControllerAdvice
-public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+@Import(ExceptionHelper.class)
+@Slf4j
+public class PnResponseEntityExceptionHandlerActivation extends PnResponseEntityExceptionHandler {
 
-    @ExceptionHandler(JsonMappingException.class)
-    public void handle(JsonMappingException e) {
-        log.error("Returning HTTP 400 Bad Request {}", e.getMessage());
+    public PnResponseEntityExceptionHandlerActivation(ExceptionHelper exceptionHelper) {
+        super(exceptionHelper);
     }
+
 
     @ExceptionHandler(PnGenericException.class)
     public Mono<ResponseEntity<Problem>> handleResponseEntityException(final PnGenericException exception){
@@ -87,28 +91,11 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
         return Mono.just(ResponseEntity.status(problem.getStatus()).body(problem));
     }
 
-
     private void settingTraceId(Problem problem){
         try {
             problem.setTraceId(MDC.get(MDCUtils.MDC_TRACE_ID_KEY));
         } catch (Exception e) {
             log.warn("Cannot get traceid", e);
         }
-    }
-
-    @Override
-    protected @NotNull Mono<ResponseEntity<Object>> handleServerWebInputException(@NotNull ServerWebInputException ex,
-                                                                                  @NotNull HttpHeaders headers, @NotNull HttpStatusCode status, @NotNull ServerWebExchange exchange) {
-        var problem = new Problem();
-        var pd = ex.getBody();
-
-        problem.setType(pd.getType().toString());
-        problem.setTitle(pd.getTitle());
-        problem.setStatus(pd.getStatus());
-        problem.setDetail(pd.getDetail());
-        problem.setTimestamp(Instant.now());
-        settingTraceId(problem);
-
-        return Mono.just(ResponseEntity.status(status.value()).body(problem));
     }
 }
--- src/test/java/it/pagopa/pn/paperchannel/exception/RestExceptionHandlerTest.java
+++ src/test/java/it/pagopa/pn/paperchannel/exception/RestExceptionHandlerTest.java
@@ -1,32 +1,27 @@
 package it.pagopa.pn.paperchannel.exception;
 
+import it.pagopa.pn.commons.exceptions.ExceptionHelper;
 import it.pagopa.pn.paperchannel.generated.openapi.server.v1.dto.PaperEvent;
-import it.pagopa.pn.paperchannel.generated.openapi.server.v1.dto.Problem;
 import it.pagopa.pn.paperchannel.model.StatusDeliveryEnum;
+import it.pagopa.pn.paperchannel.springbootcfg.PnResponseEntityExceptionHandlerActivation;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Spy;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ProblemDetail;
-import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.server.ServerWebInputException;
 import reactor.core.publisher.Mono;
 
-import java.net.URI;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static it.pagopa.pn.paperchannel.exception.ExceptionTypeEnum.*;
-import static org.mockito.Mockito.mock;
 
 
 class RestExceptionHandlerTest {
     @Spy
-    private RestExceptionHandler restExceptionHandler;
+    private PnResponseEntityExceptionHandlerActivation restExceptionHandler;
 
 
     @BeforeEach
@@ -106,42 +101,8 @@ class RestExceptionHandlerTest {
                 .block();
     }
 
-    @Test
-    void handleServerWebInputExceptionTest(){
-        URI typeUri = URI.create("https://example.com/problem");
-        String title = "Bad Request";
-        String detail = "Required parameter is missing";
-        int status = HttpStatus.BAD_REQUEST.value();
-
-        ProblemDetail problemDetail = ProblemDetail.forStatus(status);
-        problemDetail.setType(typeUri);
-        problemDetail.setTitle(title);
-        problemDetail.setDetail(detail);
-
-        ServerWebInputException serverWebInputException = new ServerWebInputException(detail);
-
-        HttpHeaders headers = new HttpHeaders();
-        HttpStatusCode httpStatusCode = HttpStatus.BAD_REQUEST;
-
-        ServerWebExchange exchange = mock(ServerWebExchange.class);
-
-        restExceptionHandler.handleServerWebInputException(serverWebInputException, headers, httpStatusCode, exchange)
-                .map(responseEntity -> {
-                    Assertions.assertNotNull(responseEntity.getBody());
-
-                    Problem problem = (Problem) responseEntity.getBody();
-                    Assertions.assertEquals(HttpStatus.BAD_REQUEST.value(), responseEntity.getStatusCode().value());
-                    Assertions.assertEquals(title, problem.getTitle());
-                    Assertions.assertEquals(status, problem.getStatus());
-                    Assertions.assertEquals(detail, problem.getDetail());
-                    Assertions.assertNotNull(problem.getTimestamp());
-
-                    return Mono.empty();
-                })
-                .block();
-    }
 
     private void initialize() {
-        restExceptionHandler = new RestExceptionHandler();
+        restExceptionHandler = new PnResponseEntityExceptionHandlerActivation(new ExceptionHelper(Optional.empty()));
     }
 }
