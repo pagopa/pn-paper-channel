a7ca217a469f2c45af35ce9166386b2531d9648c
Vincenzo Racca <vincenzo.racca@pagopa.it>
2026-06-23 16:33:34 +0200

    update pn-commons to 2.23.0-SPRINGBOOT3-PL

--- src/main/java/it/pagopa/pn/paperchannel/config/KmsConfiguration.java
+++ src/main/java/it/pagopa/pn/paperchannel/config/KmsConfiguration.java
@@ -25,7 +25,7 @@ public class KmsConfiguration {
     public KmsClient kmsClient() {
         var builder = KmsClient.builder();
 
-        if(System.getenv("AWS_REGIONCODE") == null) {
+        if(isLocal()) {
             if (Optional.ofNullable(awsConfigs.getEndpointUrl()).isPresent()) {
                 builder.endpointOverride(URI.create(awsConfigs.getEndpointUrl()));
                 Optional.ofNullable(awsConfigs.getRegionCode()).ifPresent(r -> builder.region(Region.of(r)));
@@ -41,5 +41,9 @@ public class KmsConfiguration {
     @Qualifier("kmsEncryption")
     public DataEncryption kmsEncryption(KmsClient kmsClient){
         return new KmsEncryptionImpl(kmsClient, this.properties);
+    }
+
+    private boolean isLocal() {
+        return System.getenv("AWS_REGIONCODE") == null;
     }
 }
